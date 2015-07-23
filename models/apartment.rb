fca5a18f019ebd4819995cd298d080b4ef58ac0c
mattscilipoti <matt.scilipoti@ga.co>
2015-07-23 16:10:59 -0400

    apartment: add image

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,6 +1,10 @@
 class Apartment < ActiveRecord::Base
   has_many :tenants
 
+  def image_url(width, height)
+    "http://lorempixel.com/#{width}/#{height}/city/#{id}"
+  end
+
   def rent_as_currency
     "$#{monthly_rent}"
   end
