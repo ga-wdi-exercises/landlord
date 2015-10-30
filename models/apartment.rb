7a93e8be04f32ddbbbf23ea0aa0686b055c955d9
Peter Zhang <pzhang87@gmail.com>
2015-10-30 16:55:29 -0400

    first commit with tenant and apartment classes

---
+++ models/apartment.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Apartment
+  def initialize(address, monthy_rent, sqft, num_beds, num_baths)
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = []
+  end
+
+  def add_tenant(tenant)
+    if @renters.count < @num_beds
+      @renters << tenant
+    else
+      puts "too many renters"
+    end
+  end
+
+end
+
+binding.pry
+
+puts "stfu"
