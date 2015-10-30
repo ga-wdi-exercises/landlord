831b2dc924d9c485769c995c927f70ec344260de
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-30 16:56:22 -0400

    Tenant and Apartment classes made

---
+++ tenant.rb
@@ -0,0 +1,16 @@
+class Tenant
+
+  attr_accessor :name
+  attr_accessor :age
+  attr_accessor :gender
+
+  def initialize(name, age, gender)
+
+    @name = name
+    @age = age
+    @gender = gender
+
+  end
+
+
+end
