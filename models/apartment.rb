d76fd3b6842aefa1783491bd1b5ae2cdfd491f30
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-30 17:02:41 -0400

    Initial commit

---
+++ models/apartment.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Apartment
+  def initialize address, monthly_rent, sqft,num_beds,num_baths
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = []
+  end
+
+def add_tenant renter
+  if @renters.length < @num_beds
+    @renters << renter
+  else
+    puts "Not enough room"
+  end
+end
+
+end
+
+apt = Apartment.new(121,4000,3500,3,2)
+
+binding.pry
+
+puts "program complete"
