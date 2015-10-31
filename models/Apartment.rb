168b51d071751a18758368a9ba3e21a8065cda50
ANDRE SHONUBI <andre@codedre.com>
2015-10-31 16:36:10 -0400

    First commit added models dir with tenant & apartment ruby files

---
+++ models/Apartment.rb
@@ -0,0 +1,12 @@
+class Apartment
+
+  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = renters
+  end
+
+end
