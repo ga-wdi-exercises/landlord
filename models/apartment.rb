861547ddaa511421c813dd20d25ba627f8104318
ckib16 <ckib16@gmail.com>
2015-11-01 12:19:20 -0500

    initial commit

---
+++ models/apartment.rb
@@ -0,0 +1,5 @@
+class Apartment
+  def initialize
+    
+  end
+end
---
+++ models/tenant.rb
@@ -0,0 +1,7 @@
+class Tenant
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+end
