58c56aba6ad55930620e4ac7f39f0c210353ec78
ckib16 <ckib16@gmail.com>
2015-11-01 13:15:56 -0500

    models for tenant and apartment complete

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,5 +1,26 @@
+require "pry"
+
 class Apartment
-  def initialize
-    
+
+  attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
+
+  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = renters
+  end
+
+  def add_tenant(new_tenants)
+    if renters < num_beds
+      # binding.pry
+      #need to use self.method for setter method
+      self.renters = renters + new_tenants
+    end
   end
 end
+
+new_apartment = Apartment.new('1002 Bell Street', 3000, 2000, 3, 3, 1)
+new_apartment.add_tenant(2)
--- models/tenant.rb
+++ models/tenant.rb
@@ -1,4 +1,7 @@
 class Tenant
+
+  attr_accessor :name, :age, :gender
+
   def initialize(name, age, gender)
     @name = name
     @age = age
