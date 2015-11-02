17fa0cc4f62382e725c156529e314b333af4331c
Thom Householder <thomhouseholder@gmail.com>
2015-11-01 20:25:22 -0500

    defined Apartment class

---
+++ models/apartment.rb
@@ -0,0 +1,54 @@
+class Apartment
+
+  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
+    @address=address
+    @monthly_rent=monthly_rent
+    @sqft=sqft
+    @num_beds=num_beds
+    @num_baths=num_baths
+    @renters=renters
+  end
+  # getters
+  def address
+    @address
+  end
+  def monthly_rent
+    @monthly_rent
+  end
+  def sqft
+    @sqft
+  end
+  def num_beds
+    @num_beds
+  end
+  def num_baths
+    @num_baths
+  end
+  def renters
+    @renters
+  end
+  # setters
+  def address=(new_address)
+    @address=new_address
+  end
+  def monthly_rent(monthly_rent)
+    @monthly_rent=monthly_rent
+  end
+  def sqft=(sqft)
+    @sqft=sqft
+  end
+  def num_beds=(num_beds)
+    @num_beds=num_beds
+  end
+  def num_baths=(num_baths)
+    @num_baths=num_baths
+  end
+  def renters=(renters)
+    @renters=renters
+  end
+end
+
+# tests
+mine=Apartment.new("123 Main", "1200", 400, 2,1,2)
+
+puts mine.address
