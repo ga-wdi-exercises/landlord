bc4593b9409df36b13ad1246d23c5e789e0596ce
jen pengelly <jgpengelly@gmail.com>
2015-11-02 17:40:01 -0500

    Ruby: initial setup of aparmtnets

---
+++ models/apartments.rb
@@ -0,0 +1,9 @@
+class Apartment
+  def initialize (address, monthly_rent, sqft, num_beds, num_baths, renters)
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+  end
+end
--- models/tenant.rb
+++ models/tenant.rb
@@ -1,6 +1,8 @@
 class Tenant
+  attr_accessor :name, :age, :gender
   def initialize(name, age, gender)
-  @name = name
-  @age = age
-  @gender = gender
+    @name = name
+    @age = age
+    @gender = gender
+  end
 end
