2e1becc4c058065699ccbdc522b9ef2aaa7f4cf5
jen pengelly <jgpengelly@gmail.com>
2015-11-02 09:34:49 -0500

    Update query_exercises with select queries

--- db/query_exercises.sql
+++ db/query_exercises.sql
@@ -1,9 +1,16 @@
 -- Retrieve all info on all tenants
+SELECT * FROM tenants;
 -- Retrieve the name, age, and gender of all tenants
+SELECT name, age, gender FROM tenants;
 -- Retrieve all info on all tenants older than 65
+SELECT * FROM tenants WHERE age > 65;
 -- Retrieve all info on all tenants in apartment with id 20
+SELECT * FROM tenants WHERE apartment_id = 20;
 -- Retrieve all info on all tenants in apartment with ids 20 or 15
+SELECT * FROM tenants WHERE apartment_id = 20 OR apartment_id = 15;
+
 -- Delete all tenants whose age is greater than 65
+-- DELETE * FROM tenants WHERE age > 65;
 -- Create one new tenant, put them in any apartment you want
 -- Find just the ids for all apartments
 -- Find all info for apartments whose price is greater than $2300
