87de1f7325ae5c2da98f5d7aed8215bab5aacf5c
jen pengelly <jgpengelly@gmail.com>
2015-11-02 17:42:48 -0500

    Ruby: update apartments to include attr_accessor and add renters as instance variable.

--- models/apartments.rb
+++ models/apartments.rb
@@ -1,9 +1,11 @@
 class Apartment
+  attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
   def initialize (address, monthly_rent, sqft, num_beds, num_baths, renters)
     @address = address
     @monthly_rent = monthly_rent
     @sqft = sqft
     @num_beds = num_beds
     @num_baths = num_baths
+    @renters = renters
   end
 end
