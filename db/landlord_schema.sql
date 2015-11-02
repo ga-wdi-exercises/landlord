6656b083e27f1a1870e3e4b461f999025bcc99ac
ANDRE SHONUBI <andre@codedre.com>
2015-11-02 17:44:11 -0500

    changed name and fixed errors

---
+++ db/landlord_schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
+
+CREATE TABLE tenants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  gender VARCHAR(255),
+  apartment_id INTEGER
+);
+
+CREATE TABLE apartments (
+  id SERIAL PRIMARY KEY,
+  address TEXT,
+  monthly_rent INTEGER,
+  sqft INTEGER,
+  num_beds INTEGER,
+  num_baths INTEGER
+);
