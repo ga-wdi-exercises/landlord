e5b7c1a7850ca68ba9c2ed3647ab7468b56739bf
alea qawiyy <aq1984@gmail.com>
2015-11-03 16:50:11 -0500

    done

--- models/tenant.rb
+++ models/tenant.rb
@@ -1,3 +1,8 @@
+class Tenant <ActiveRecord::Base
+belongs_to :apartment
+end
+
+
 class tenant
       def initialize(name, age, gender)
     @name = name
@@ -7,29 +12,4 @@ class tenant
 
 end
 
-class apartment
-  def initialize(
-    address,
-    monthly_rent,
-    sqft,
-    num_beds,
-    num_baths,
-    renter
-    )
-    @address = address
-    @monthly_rent = monthly_rent
-    @sqft = sqft
-    @num_beds = num_beds
-    @num_baths = num_baths
-    @renter []
-  end
-
-#create add tenant setter
-  def add_tenant
-    if @@tenant < @num_beds
-        @renter << tenant
-      else @tenants = tenants
-    end
-  end
-
-end
+#ActiveRecord
