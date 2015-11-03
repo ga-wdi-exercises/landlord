112eac335f6d0a868bb9729411cd19fa2d826714
Brendan Mahoney <BMahoney08@gmail.com>
2015-11-02 21:58:43 -0500

    completed DB exercises

---
+++ models/tenant.rb
@@ -0,0 +1,10 @@
+class Tenant
+    attr_accessor :name
+    attr_accessor :age
+    attr_accessor :gender
+  def initialize name, age, gender
+    @name = name
+    @age = age
+    @gender = gender
+  end
+end
