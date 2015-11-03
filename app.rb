2d6291245472178886c13a941bc0c0143cd27373
Chase Gruber <chasegruber@gmail.com>
2015-11-03 16:31:26 -0500

    reconfigure seed data to run outside app.rb

--- app.rb
+++ app.rb
@@ -6,7 +6,5 @@ require "pry"
 require_relative "db/connection"
 require_relative "models/apartments"
 require_relative "models/tenants"
-require_relative "db/seeds.rb"
-
 
 binding.pry
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+require "bundler/setup"
+require "pg"
+require "active_record"
+require "pry"
+
+require_relative "../db/connection"
+require_relative "../models/apartments"
+require_relative "../models/tenants"
+
+Apartment.destroy_all
+Tenant.destroy_all
+
 Apartment.create(address:"123 ABC Street", monthly_rent:100000000, sqft:200, num_beds:12, num_baths:0)
 Apartment.create(address:"4000 Dead End", monthly_rent:1000, sqft:20000, num_beds:2, num_baths:10)
 Apartment.create(address:"1 No Name", monthly_rent:10, sqft:40, num_beds:5, num_baths:5)
