c4092e12c6ae8fd289811ed9083eec6861dac578
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-03 08:56:21 -0500

    first commit of schema.sql and ruby folder

---
+++ ruby/apartment.rb
@@ -0,0 +1,10 @@
+class Apartment
+  def initialize (address, monthy_rent, sqft, num_beds, num_baths, renters)
+    @address = address
+    @monthy_rent = monthy_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @renters = renters
+  end
+
+end
