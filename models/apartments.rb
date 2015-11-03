a2b4779d350f42ca2aa7e7eb6ca075099d0841b2
Chase Gruber <chasegruber@gmail.com>
2015-11-03 16:17:02 -0500

    clean up workspace

--- models/apartments.rb
+++ models/apartments.rb
@@ -1,6 +1,6 @@
 class Apartment < ActiveRecord::Base
   has_many :tenants
-  # attr_accessor :renters, :rent #tenants?
+  # attr_accessor :renters, :rent
   # attr_reader :address, :sqft, :num_beds, :num_baths
   # def initialize (address,rent,sqft,num_beds,num_baths,renters)
   #   @address = address
@@ -16,5 +16,3 @@ class Apartment < ActiveRecord::Base
   #   end
   # end
 end
-
-# apt = Apartment.new("2501 Porter St",2000,100,1,1,1)
