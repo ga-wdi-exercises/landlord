61ed8703deefda1b8124d8f4f0835b02464c0603
Matiullah Amin <matiullah243@gmail.com>
2015-11-02 20:00:49 -0500

    resubmitting

---
+++ models/apartments.rb
@@ -0,0 +1,18 @@
+class apartment
+
+  def initialize (address, monthly_rent, sqft, num_beds, num_baths, renters)
+    @address = address
+    @monthly_rent = monthly_rent
+    @sqft = sqft
+    @num_beds = num_beds
+    @num_baths = num_baths
+    @renters = renters
+
+  end
+
+  def add_tenant
+    while renters <= num_beds
+      renters += 1
+  end
+
+end
---
+++ models/tenants.rb
@@ -0,0 +1,7 @@
+class tenant
+
+  def initialize (name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
