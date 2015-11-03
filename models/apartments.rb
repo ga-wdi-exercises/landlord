bc00591c395f926286db9d3424c6e38d551e7f6b
Chase Gruber <chasegruber@gmail.com>
2015-11-03 15:58:44 -0500

    make gemfile and connection files

--- models/apartments.rb
+++ models/apartments.rb
@@ -1,19 +1,19 @@
-class Apartment
-  attr_accessor :renters, :rent #tenants?
-  attr_reader :address, :sqft, :num_beds, :num_baths
-  def initialize (address,rent,sqft,num_beds,num_baths,renters)
-    @address = address
-    @rent = rent
-    @sqft = sqft
-    @num_beds = num_beds
-    @num_baths = num_baths
-    @renters = renters
-  end
-  def add_tenant (tenant)
-    if @renters.length < @num_beds
-      @renters << tenant
-    end
-  end
+class Apartment < ActiveRecord::Base
+  # attr_accessor :renters, :rent #tenants?
+  # attr_reader :address, :sqft, :num_beds, :num_baths
+  # def initialize (address,rent,sqft,num_beds,num_baths,renters)
+  #   @address = address
+  #   @rent = rent
+  #   @sqft = sqft
+  #   @num_beds = num_beds
+  #   @num_baths = num_baths
+  #   @renters = renters
+  # end
+  # def add_tenant (tenant)
+  #   if @renters.length < @num_beds
+  #     @renters << tenant
+  #   end
+  # end
 end
 
 # apt = Apartment.new("2501 Porter St",2000,100,1,1,1)
--- models/tenants.rb
+++ models/tenants.rb
@@ -1,8 +1,8 @@
-class Tenant
-  attr_reader :name, :age, :gender
-  def initialize(name, age, gender)
-    @name = name
-    @age = age
-    @gender = gender
-  end
+class Tenant < ActiveRecord::Base
+  # attr_reader :name, :age, :gender
+  # def initialize(name, age, gender)
+  #   @name = name
+  #   @age = age
+  #   @gender = gender
+  # end
 end
