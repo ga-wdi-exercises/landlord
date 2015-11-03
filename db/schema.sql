dc5d942344caec5d401e28421e31a39a2aed5c45
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-11-03 09:16:04 -0500

    schema finished and added, seeds added, and query exercises finished

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,5 @@
-CREATE TABLE tenants(
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  age INT,
-  gender TEXT,
-  aparment_id INT FOREIGN KEY
-);
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
 
 CREATE TABLE apartments(
   id SERIAL PRIMARY KEY,
@@ -13,4 +8,12 @@ CREATE TABLE apartments(
   sqft INT,
   num_beds INT,
   num_baths INT
+);
+
+CREATE TABLE tenants(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INT,
+  gender TEXT,
+  apartment_id INT
 );
