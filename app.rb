3d8cef97304b311b46e5d5b57e7f0da046737b05
Rachel Porter <rachelandrewsporter@gmail.com>
2015-11-03 20:19:33 -0500

    adds commandline app to enable landlord to enter a new tenant or apartment

--- app.rb
+++ app.rb
@@ -7,4 +7,47 @@ require_relative "db/connection" # require the db connection file that connects 
 require_relative "models/apartment"
 require_relative "models/tenant"
 
-binding.pry
+def landlord_clapp
+  puts "Hello Landlord. Type 'tenant' or 'apartment' to create new:"
+  type = gets.chomp
+  if type == 'apartment'
+    create_apartment
+  elsif type == 'tenant'
+    create_tenant
+  else
+    puts "I'm sorry, I don't understand your input."
+    landlord_clapp
+  end
+end
+
+def create_apartment
+  puts "What is the apartment address?"
+  address = gets.chomp
+  puts "What is the apartment's rent?"
+  monthly_rent = gets.chomp
+  puts "What is the apartment's square footage?"
+  sqft = gets.chomp
+  puts "How many bedrooms?"
+  num_beds = gets.chomp
+  puts "How many bathrooms?"
+  num_baths = gets.chomp
+
+  new_apartment = Apartment.create(address: address, monthly_rent: monthly_rent.to_i, sqft: sqft.to_i, num_beds: num_beds.to_i, num_baths: num_baths.to_i)
+  puts "You have created a new apartment at the address " + new_apartment.address
+  "New apartment id: " + new_apartment.id.to_s
+end
+
+def create_tenant
+  puts "What is the tenant's full name?"
+  name = gets.chomp
+  puts "What is the tenant's age?"
+  age = gets.chomp
+  puts "What is the tenant's gender?"
+  gender = gets.chomp
+
+  new_tenant = Tenant.create(name: name, age: age.to_i, gender: gender)
+  puts "You have created a new tenant named " + new_tenant.name
+  "New tenant id: " + new_tenant.id.to_s
+end
+
+landlord_clapp
