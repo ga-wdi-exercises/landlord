3351bc812f84ab15762c42e41479efaba7f0e4f9
Elise Kain <elise.kain@gmail.com>
2015-11-03 19:04:16 -0500

    Add psuedo HTML for views

--- app.rb
+++ app.rb
@@ -1,11 +1,32 @@
-require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
+# Home
 get "/" do
-  erb :index
+  erb :"index"
 end
 
+# Show all apartments
 get "/apartments" do
-  erb :apartments
+  erb :"apartments/index"
+end
+
+# Add apartment
+get "/apartments/new" do
+  erb :"apartments/new"
+end
+
+# Show apartment details by id
+get "/apartments/:apt_id" do
+  erb :"apartments/show"
+end
+
+# Show all tenants in apartment (by apt_id)
+get "/apartments/:apt_id/tenants" do
+  erb :"tenants/index"
+end
+
+# Show all tenants in apartment (by apt_id)
+get "/apartments/:apt_id/tenants/new" do
+  erb :"tenants/new"
 end
