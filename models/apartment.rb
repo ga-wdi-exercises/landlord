f39c9d0eaa21a5c6177afd96814264192121e932
Jared Nichols <jnichols300@gmail.com>
2015-11-04 16:18:48 -0500

    Active Record Homework Pro-seeding  hahahaha

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,25 +1,31 @@
-require 'pry'
-
-class Apartments
- attr_accessor :address
- attr_accessor :monthly_rent
- attr_accessor :sqft
- attr_accessor :num_beds
- attr_accessor :num_baths
- attr_accessor :renter
-
-  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
-    @address = address
-    @monthy_rent = monthy_rent
-    @sqft = sqft
-    @num_beds = num_beds
-    @num_baths = num_baths
-    @renters = []
-  end
+class Apartment < ActiveRecord::Base
+  has_many :tenants
 end
 
-def add_tenant (person)
-  if @renters.length < @num_beds
-    @renters << person
-  end
-end
+
+
+# require 'pry'
+#
+# class Apartments
+#  attr_accessor :address
+#  attr_accessor :monthly_rent
+#  attr_accessor :sqft
+#  attr_accessor :num_beds
+#  attr_accessor :num_baths
+#  attr_accessor :renter
+#
+#   def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
+#     @address = address
+#     @monthy_rent = monthy_rent
+#     @sqft = sqft
+#     @num_beds = num_beds
+#     @num_baths = num_baths
+#     @renters = []
+#   end
+# end
+#
+# def add_tenant (person)
+#   if @renters.length < @num_beds
+#     @renters << person
+#   end
+# end
--- models/tenant.rb
+++ models/tenant.rb
@@ -1,13 +1,17 @@
-require 'pry'
-
-class Tenants
-  attr_accessor :name
-  attr_accessor :age
-  attr_accessor :gender
-
-  def initialize(name, age, gender)
-    @name = name
-    @age = age
-    @gender = gender
-  end
+class Tenant < ActiveRecord::Base
+  belongs_to :apartment
 end
+
+# require 'pry'
+# 
+# class Tenants
+#   attr_accessor :name
+#   attr_accessor :age
+#   attr_accessor :gender
+#
+#   def initialize(name, age, gender)
+#     @name = name
+#     @age = age
+#     @gender = gender
+#   end
+# end
