c3a5430e1a3e1c6a25ab0057ebc675db266f8ce2
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-11-04 08:49:24 -0500

    Add command line application

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require "pg"
 require "active_record"
 require "pry"
 
+
 require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
+
 
 binding.pry
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,16 @@ Tenant.destroy_all
 shirlington = Apartment.create(address: "3000 South Randolph Street, Arlington, VA 22206", monthly_rent: 2100, sqft: 1040, num_beds: 2, num_baths: 2)
 clarendon  = Apartment.create(address: "3129 8th Street N, Arlington, VA 22201", monthly_rent: 3000, sqft: 1500, num_beds: 4, num_baths: 3)
 cameo = Apartment.create(address: "5008 Cameo Terrace, Perry Hall, MD 21128", monthly_rent: 4000, sqft: 3000, num_beds: 4, num_baths: 4)
-cameo.tenants.create(name: "Becky Beauchamp", age: 25, gender: "Female")
-cameo.tenants.create(name: "Carol Beauchamp", age: 58, gender: "Female")
-clarendon.tenants.create(name: "Anna Boeri", age: 25, gender: "Female")
-clarendon.tenants.create(name: "Josh More", age: 27, gender: "Male")
-clarendon.tenants.create(name: "Emilie Boeri", age: 25, gender: "Female")
-shirlington.tenants.create(name: "Annie Unger", age: 26, gender: "Female")
-Tenant.create(name: "Erin Purcell", age: 25, gender: "Female")
-Tenant.create(name: "Bryan Beauchamp", age: 59, gender: "Male")
-Tenant.create(name: "Alice Weber", age: 60, gender: "Female")
-clarendon.tenants.create(name: "Steve Cooler", age: 30, gender: "Male")
+becky = cameo.tenants.create(name: "Becky Beauchamp", age: 25, gender: "Female")
+carol = cameo.tenants.create(name: "Carol Beauchamp", age: 58, gender: "Female")
+anna = clarendon.tenants.create(name: "Anna Boeri", age: 25, gender: "Female")
+josh = clarendon.tenants.create(name: "Josh More", age: 27, gender: "Male")
+emilie = clarendon.tenants.create(name: "Emilie Boeri", age: 25, gender: "Female")
+annie = shirlington.tenants.create(name: "Annie Unger", age: 26, gender: "Female")
+erin = Tenant.create(name: "Erin Purcell", age: 25, gender: "Female")
+bryan = Tenant.create(name: "Bryan Beauchamp", age: 59, gender: "Male")
+alice = Tenant.create(name: "Alice Weber", age: 60, gender: "Female")
+steve = clarendon.tenants.create(name: "Steve Cooler", age: 30, gender: "Male")
 
 all_tenants = Tenant.all
 shirlington= Apartment.find_by(address: "3000 South Randolph Street, Arlington, VA 22206")
@@ -35,6 +35,5 @@ all_tenants_of_shirlington = all_tenants_of_cameo
 last_apartment = Apartment.last
 last_apartment.address = "450"
 last_apartment.save
-erin = Tenant.find_by(name: "Erin Purcell")
 erin.update(name: "Emily Purcell")
 last_apartment.destroy
