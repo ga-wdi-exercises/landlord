4fe9a49d5f54002fb84c4fff5671b5d7fbfff7b8
Markham Shofner <markham.shofner@gmail.com>
2015-11-03 19:08:45 -0500

    adjust apartment and tenant classes to initialize, based on passed in attributes

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,3 +1,10 @@
 class Apartment
-
-end 
+  def initialize (address, monthly_rent, sqft, num_beds, num_baths, renter_id)
+    @address = address,
+    @monthly_rent = monthly_rent,
+    @sqft = sqft,
+    @num_beds = num_beds,
+    @num_baths = num_baths,
+    @renter_id = renter_id
+  end
+end
--- models/tenant.rb
+++ models/tenant.rb
@@ -1,3 +1,8 @@
 class Tenant
-
-end 
+  def initialize (name, age, gender, apartment_id)
+    @name = name,
+    @age = age,
+    @gender = gender,
+    @apartment_id = apartment_id
+  end
+end
