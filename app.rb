4f88feab23521f18cdca711a10d69e7bb0d06c95
jen pengelly <jgpengelly@gmail.com>
2015-11-03 21:50:15 -0500

    update erb files for apartment and add tenant erb files.

--- app.rb
+++ app.rb
@@ -12,27 +12,29 @@ get '/apartments' do
   erb :"apartments/index"
 end
 
-# View a single apartment's details
-get '/apartments/:id' do
- erb :"apartments/apartment_id"
-end
-
 # Create a new apartment - should I use post?
 # Route will show a form for adding a new apartment
 # Form includes address, monthly_rent, sqft, num_beds, num_baths, renters
+# this has to come before id
 get '/apartments/new' do
   puts " 💠 " * 12
-  erb :"apartments/new_apt"
+  erb :"apartments/new"
 end
 
-# # List all tenants for apartment 1
-# get '/tenants/:apartment_id' do
-#   erb :"tenants"
-# end
-#
-# # Create a new tenant (in apartment 1?)
-# # Route will show a form for adding a new tenant
-# # Form includes Name, Age, Gender
-# get '/tenants/new' do
-#   erb :"tenants/new"
-# end
+# View a single apartment's details
+get '/apartments/:id' do
+ erb :"apartments/apt_id"
+end
+
+# Create a new tenant (in apartment 1?)
+# Route will show a form for adding a new tenant
+# Form includes Name, Age, Gender
+get '/apartments/:id/tenants/new' do
+  erb :"tenants/new"
+end
+
+
+# List all tenants for apartment 1
+get '/apartments/:id/tenants' do
+  erb :"tenants/index"
+end
