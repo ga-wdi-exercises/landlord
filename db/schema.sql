7e2682b432d08d3ceff8b5d378824958714a7d70
Josh Cohen <jacohen10@EMAIL.COM>
2015-11-04 09:00:43 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
+
+
+CREATE TABLE tenants (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  apartment_id INTEGER
+);
+
+CREATE TABLE apartments (
+  id SERIAL PRIMARY KEY,
+  address TEXT,
+  monthly_rent INTEGER,
+  sqft INTEGER,
+  num_beds INTEGER,
+  num_baths INTEGER
+);
---
+++ models/tenant.rb
@@ -0,0 +1,9 @@
+class Tenant
+
+  def initialize name, age, gender
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+end
