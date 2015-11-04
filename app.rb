783f249182920b9bc9d38e58750ce7d2eb4a4a42
Thomas Solow <thomassolow@gmail.com>
2015-11-04 08:38:26 -0500

    added comments and changed some stuff around

--- app.rb
+++ app.rb
@@ -7,30 +7,32 @@ require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
 
+#this function adds a new apartment to the db
 def add_apt
   apt_hash = Hash.new(0)
-  puts "Enter information for your new apartment here.  If you don't know a value enter null.  Type exit or quit to quit."
+  puts "Enter information for your new apartment here.  If you don't know a value press enter to get to the next field.  Type exit or quit to quit."
   puts "Enter an address"
   address = gets.chomp
   if address == "exit" || address == "quit"
     return
   end
-  apt_hash[:address]= address unless address == "null"
+  apt_hash[:address]= address unless address == ""
   puts "Enter the monthly rent"
   rent = gets.chomp
-  apt_hash[:monthly_rent]= rent unless rent == "null"
+  apt_hash[:monthly_rent]= rent unless rent == ""
   puts "Enter the square feet"
   sqft = gets.chomp
-  apt_hash[:sqft]= sqft unless sqft == "null"
+  apt_hash[:sqft]= sqft unless sqft == ""
   puts "Enter number of beds"
   num_beds = gets.chomp
-  apt_hash[:num_beds]= num_beds unless num_beds == "null"
+  apt_hash[:num_beds]= num_beds unless num_beds == ""
   puts "Enter number of bathrooms"
   num_baths = gets.chomp
-  apt_hash[:num_baths]= num_baths unless num_baths == "null"
+  apt_hash[:num_baths]= num_baths unless num_baths == ""
   Apartment.create(apt_hash)
   puts "Apartment added!"
 end
+#this function deletes an apt from the db
 def delete_apt
   puts "Enter the address of the apartment you would like to delete. Type quit or exit to leave. NOTE: deleting an apartment will delete all tenants of that apartment from the system."
   user_input = gets.chomp
@@ -38,16 +40,25 @@ def delete_apt
     return
   else
     apt = Apartment.find_by(address: user_input)
+    #check to see if apt address works
+    if (apt == nil)
+      puts "Invalid address."
+      return
+    end
+    #remove all tenants from that apt
     apt.tenants.delete
+    #delete the apt
     apt.delete
     puts "Apartment deleted"
   end
 end
+#this function holds all the function calls to manage tenants
 def manage_tenants
   puts "To add a tenant to the system type add tenant.  To delete a tenant type delete tenant.  To modify an existing tenant, type modify tenant.  Or enter exit or quit."
   user_input = gets.chomp
   if user_input == "quit" || user_input == "exit"
     return
+  #calls the three functions to handle tenants based on the user input
   elsif user_input == "add tenant"
     plus_tenant
   elsif user_input == "delete tenant"
@@ -56,56 +67,67 @@ def manage_tenants
     mod_tenant
   end
 end
+#this function adds a new tenant
 def plus_tenant
-  puts "Enter information about the new tenant below.  Enter null as a placeholder field.  Type exit or quit to leave."
+  puts "Enter information about the new tenant below.  If you don't know a field press enter to get to the next field.  Type exit or quit to leave."
   puts "Name:"
   user_input = gets.chomp
   if user_input == "quit" || user_input == "exit"
     return
   end
   new_tenant = Hash.new(0)
-  new_tenant[:name]=user_input unless user_input == "null"
+  new_tenant[:name]=user_input unless user_input == ""
   puts "Age:"
   user_input = gets.chomp
-  new_tenant[:age]=user_input unless user_input == "null"
+  new_tenant[:age]=user_input unless user_input == ""
   puts "Gender:"
   user_input = gets.chomp
-  new_tenant[:gender]= user_input unless user_input == "null"
+  new_tenant[:gender]= user_input unless user_input == ""
   puts "Apartment Address:"
   user_input = gets.chomp
-  new_tenant[:apartment_id]= Apartment.find_by(address: user_input).id unless user_input == "null"
-  puts new_tenant
+  if Apartment.find_by(address: user_input) != nil
+    new_tenant[:apartment_id]= Apartment.find_by(address: user_input).id unless user_input == "null"
+  else
+    puts "Invalid Apartment input.  Entering a null value.  You may edit this value later."
+  end
   Tenant.create(new_tenant)
   puts "Tenant added!"
 end
+#this function modifies an existing tenant
 def mod_tenant
   puts "Enter the name of the tenant you would like to modify.  Or type exit or quit to leave"
   user_input = gets.chomp
   if user_input == "quit" || user_input == "exit"
     return
   end
+  #find the tenant to modify, make sure he exists
   mod_tenant = Tenant.find_by(name: user_input)
+  if mod_tenant == nil
+    puts "Couldn't find that tenant.  You can add a new tenant elsewhere."
+    return
+  end
   mod_hash = Hash.new(0)
-  puts "Enter new values for each field.  Or enter null to leave a field unchanged."
+  puts "Enter new values for each field.  Or press enter to leave a field unchanged."
   puts "Name: #{mod_tenant.name}"
   puts "New name:"
   user_input = gets.chomp
-  mod_hash[:name] = user_input unless user_input == "null"
+  mod_hash[:name] = user_input unless user_input == ""
   puts "Age: #{mod_tenant.age}"
   puts "New age:"
   user_input = gets.chomp
-  mod_hash[:age] = user_input unless user_input == "null"
+  mod_hash[:age] = user_input unless user_input == ""
   puts "Gender: #{mod_tenant.gender}"
   puts "New gender:"
   user_input = gets.chomp
-  mod_hash[:gender] = user_input unless user_input == "null"
+  mod_hash[:gender] = user_input unless user_input == ""
   puts "Apartment: #{Apartment.find(mod_tenant.apartment_id).address}"
   puts "New apartment (address):"
   user_input = gets.chomp
-  mod_hash[:apartment_id] = Apartment.find_by(address: user_input).id unless user_input == "null"
+  mod_hash[:apartment_id] = Apartment.find_by(address: user_input).id unless user_input == ""
   mod_tenant.update(mod_hash)
   puts "Tenant information updated!"
 end
+#remove a tenant with this function
 def remove_tenant
   puts "Enter a tenant name.  Or exit/quit"
   user_input = gets.chomp
@@ -113,9 +135,15 @@ def remove_tenant
     return
   end
   bye_tenant = Tenant.find_by(name: user_input)
+  #make sure tenant exists
+  if bye_tenant == nil
+    puts "Couldn't find that tenant.  Feel free to try again."
+    return
+  end
   bye_tenant.delete
   puts "Tenant Removed!"
 end
+#this function modifies an apartment
 def mod_apt
   puts "Enter the apartment address.  Enter quit or exit to leave"
   user_input = gets.chomp
@@ -124,6 +152,11 @@ def mod_apt
   end
   mod_hash = Hash.new(0)
   mod_apt = Apartment.find_by(address: user_input)
+  #checks to see if apartment exists
+  if mod_apt == nil
+    puts "Couldn't find an apartment with this address."
+    return
+  end
   puts "Enter a new value for each attribute.  If you don't want to modify a certain attribute, enter null"
   puts "Adress:"
   user_input = gets.chomp
@@ -143,6 +176,7 @@ def mod_apt
   mod_apt.update(mod_hash)
   puts "Apartment updated!"
 end
+#this function holds all the sub-functions used to manage apartments.
 def manage_apts
   loop do
     puts "To add a new apartment, type new apt.  To delete an apartment type delete apt.  To edit exiting apartments, type modify apt. To leave, type exit or quit."
@@ -164,6 +198,7 @@ def manage_apts
     end
   end
 end
+#this is the main command line app from which all other functions are called
 def run_cmd_app
   loop do
     puts "Enter quit or exit to leave.  Enter apts or apartments to manage apartments.  Enter ten or tenants to manage tenants."
@@ -182,4 +217,6 @@ def run_cmd_app
     end
   end
 end
+
+#run the app
 run_cmd_app
