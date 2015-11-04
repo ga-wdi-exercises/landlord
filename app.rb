a978536f1e042fe92ce3d26fc1f850aac4cd7f34
Tyler <tylerscottcrosse@gmail.com>
2015-11-03 19:46:23 -0500

    updated app.rb with GETS and created corresponding .erb files

--- app.rb
+++ app.rb
@@ -1,36 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+get '/' do
+  erb :index
+end
+
 # The homepage should list several menu options
 # List all apartments (a link to GET /apartments)
 get '/apartments' do
 # The route GET /apartments should list all apartments
   # these apartments will just be hardcoded in your app.rb or in your erb file.
-
-end
-
-# View an apartment's details(a link to GET /apartments/1)
-get '/apartments/1' do
-# The route GET /apartments/1 should show info about a single apartment
-  # Tell the user the address, monthly_rent, sqft, num_beds, num_baths, and renters
-
+  erb :apartment_index
 end
 
 # Add an apartment(a link to GET /apartments/new)
 get '/apartments/new' do
 # The route GET /apartments/new should show a form for adding a new apartment
   # Make sure to get the appropriate input from the user when creating an apartment as per schema
+  erb :apartment_new
+end
 
+# View an apartment's details(a link to GET /apartments/1)
+get '/apartments/:id' do
+# The route GET /apartments/1 should show info about a single apartment
+  # Tell the user the address, monthly_rent, sqft, num_beds, num_baths, and renters
+  erb :apartment_id
 end
 
 # List tenants (a link to GET /apartments/1/tenants)
-get '/apartments/1/tenants' do
+get '/apartments/:id/tenants' do
 # The route GET /apartments/1/tenants should list all tenants for 1 apartment.
-
+  erb :tenants_index
 end
 
-get '/apartments/1/tenants/new' do
+get '/apartments/:id/tenants/new' do
 # The route GET /apartments/1/tenants/new should show a form for adding a new tenant.
   # Make sure to get the appropriate input from the user to create your person as per schema
-
+  erb :tenant_new
 end
