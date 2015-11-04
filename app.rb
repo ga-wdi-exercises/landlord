86ed0094d7517982203a03555c74b2a7c03b43a2
jen pengelly <jgpengelly@gmail.com>
2015-11-03 21:10:01 -0500

    spelling corrections. commented out code for testing. add puts statement for testing

--- app.rb
+++ app.rb
@@ -3,34 +3,38 @@ require 'sinatra/reloader'
 
 # homepage
 get '/' do
+  puts " 💠 " * 12
   erb :"index"
 end
 
+
 # List all apartments (hardcode into the erb file)
-get '/apartments' do
-  erb :"aparmtments"
+get '/apartments/index' do
+  puts " 💠 " * 12
+  erb :"apartments/index"
 end
 
-# View a single apartment's details
-get '/apartments/:id' do
- erb :"apartments"
-end
-
-# Create a new apartment - should I use post?
-# Route will show a form for adding a new apartment
-# Form includes address, monthly_rent, sqft, num_beds, num_baths, renters
-get 'apartments/new' do
-  erb :"apartments/new"
-end
-
-# List all tenants for apartment 1
-get '/tenants/:apartment_id' do
-  erb :"tenants"
-end
-
-# Create a new tenant (in apartment 1?)
-# Route will show a form for adding a new tenant
-# Form includes Name, Age, Gender
-get '/tenants/new' do
-  erb :"tenants/new"
-end
+# # View a single apartment's details
+# get '/apartments/:id' do
+  # puts " 💠 " * 12
+#  erb :"apartments"
+# end
+#
+# # Create a new apartment - should I use post?
+# # Route will show a form for adding a new apartment
+# # Form includes address, monthly_rent, sqft, num_beds, num_baths, renters
+# get 'apartments/new' do
+#   erb :"apartments/new"
+# end
+#
+# # List all tenants for apartment 1
+# get '/tenants/:apartment_id' do
+#   erb :"tenants"
+# end
+#
+# # Create a new tenant (in apartment 1?)
+# # Route will show a form for adding a new tenant
+# # Form includes Name, Age, Gender
+# get '/tenants/new' do
+#   erb :"tenants/new"
+# end
