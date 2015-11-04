7b9901aa3db2b0bbb4e77e22790433d135854abf
jen pengelly <jgpengelly@gmail.com>
2015-11-03 20:21:20 -0500

    updated app.rb with pseudo code for find two queries. Spelling corrections.

--- app.rb
+++ app.rb
@@ -3,34 +3,34 @@ require 'sinatra/reloader'
 
 # homepage
 get '/' do
-  erb :index
+  erb :"index"
 end
 
 # List all apartments (hardcode into the erb file)
 get '/apartments' do
-  erb :aparmtments
+  erb :"aparmtments"
 end
 
-# View a single aparments details
-get '/apartments/:apartment_id' do
- erb :apartments
+# View a single apartment's details
+get '/apartments/:id' do
+ erb :"apartments"
 end
 
 # Create a new apartment - should I use post?
 # Route will show a form for adding a new apartment
 # Form includes address, monthly_rent, sqft, num_beds, num_baths, renters
-get 'aparmtments/new' do
-  erb :apartments
+get 'apartments/new' do
+  erb :"apartments/new"
 end
 
 # List all tenants for apartment 1
-get '' do
-  erb :tenants
+get '/tenants/:apartment_id' do
+  erb :"tenants"
 end
 
 # Create a new tenant (in apartment 1?)
 # Route will show a form for adding a new tenant
 # Form includes Name, Age, Gender
-get '' do
-  erb :tenants
+get '/tenants/new' do
+  erb :"tenants/new"
 end
