12220a612df444d080fd725173262d3e7829dfc4
Elise Kain <elise.kain@gmail.com>
2015-11-04 23:10:36 -0500

    Remove initialize methods from models

--- models/tenant.rb
+++ models/tenant.rb
@@ -2,11 +2,4 @@ require "pry"
 
 class Tenant < ActiveRecord::Base
   belongs_to :apartment
-  # attr_accessor :name, :age, :gender
-  #
-  # def initialize (name, age, gender)
-  #   @name = name
-  #   @age = age
-  #   @gender = gender
-  # end
 end
