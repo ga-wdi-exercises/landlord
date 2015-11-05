95868a8ef7824ed90c51e184591acb84fd94ac2b
Elise Kain <elise.kain@gmail.com>
2015-11-05 15:26:54 -0500

    Create CRUD functionality for apt & tenants

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -11,28 +11,31 @@ end
 
 # Post new apartment form
 post '/apartments' do
-  @new_apt = Apartment.create(address: params[:address], monthly_rent: params[:monthly_rent], sqft: params[:sqft], num_beds: params[:num_beds], num_baths: params[:num_beds])
-  redirect "/apartments/" + @new_apt.id.to_s
+  apt = Apartment.create(address: params[:address], monthly_rent: params[:monthly_rent], sqft: params[:sqft], num_beds: params[:num_beds], num_baths: params[:num_baths])
+  redirect "/apartments/" + apt.id.to_s
 end
 
 # Edit apartment details
 get "/apartments/:apt_id/edit" do
-  @apartments = Apartment.all
-  @apt_id = params[:apt_id].to_i
-  @apt_details = Apartment.find(@apt_id)
+  @apt_details = Apartment.find(params[:apt_id])
   erb :"apartments/edit"
+end
+
+# Patch (Edit) apartment form
+patch '/apartments/:apt_id' do
+  apt = Apartment.find(params[:apt_id])
+  apt.update(address: params[:address], monthly_rent: params[:monthly_rent], sqft: params[:sqft], num_beds: params[:num_beds], num_baths: params[:num_baths])
+  redirect "/apartments/" + params[:apt_id]
 end
 
 # Show apartment details by id
 get "/apartments/:apt_id" do
-  @apartments = Apartment.all
-  @apt_id = params[:apt_id]
   @apt_details = Apartment.find(params[:apt_id])
   erb :"apartments/show"
 end
 
 # Delete apartment
-delete "/apartments/:apt_id/delete" do
+delete "/apartments/:apt_id" do
   Apartment.destroy(params[:apt_id])
   redirect "/apartments"
 end
--- controllers/tenants.rb
+++ controllers/tenants.rb
@@ -11,21 +11,34 @@ end
 
 # Post new tenant form
 post '/tenants' do
-  @new_tenant = Tenant.create(name: params[:name], age: params[:age], gender: params[:gender])
-  redirect '/tenants/' + @new_tenant.id.to_s
+  apt = Apartment.find_by(address: params[:apt])
+  new_tenant = Tenant.create(name: params[:name], age: params[:age], gender: params[:gender], apartment_id: apt.id)
+  redirect '/tenants/' + new_tenant.id.to_s
 end
 
+
+# Delete Tenant
+delete "/tenants/:tenant_id" do
+  Tenant.destroy(params[:tenant_id])
+  redirect "/tenants"
+end
+
+# Show tenant
 get "/tenants/:tenant_id" do
-  @tenants = Tenant.all
-  @tenant_id = params[:tenant_id]
-  @tenant_details = Tenant.find(@tenant_id)
+  @tenant_details = Tenant.find(params[:tenant_id])
   erb :"tenants/show"
 end
 
 # Edit tenant details
 get "/tenants/:tenant_id/edit" do
-  @tenants = Tenant.all
-  @tenant_id = params[:tenant_id]
-  @tenant_details = Tenant.find(@tenant_id)
+  @tenant_details = Tenant.find(params[:tenant_id])
   erb :"tenants/edit"
+end
+
+# Submit tenant edits to db
+patch "/tenants/:tenant_id/edit" do
+  tenant = Tenant.find(params[:tenant_id])
+  apt = Apartment.find_by(address: params[:apt])
+  tenant.update(name: params[:name], age: params[:age], gender: params[:gender], apartment_id: apt.id)
+  redirect "/tenants/" + tenant.id.to_s
 end
