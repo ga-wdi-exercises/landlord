3a2f662fc2f12c2ca64cd89de119f8741c046181
Timothy Gregg <timtgregg@gmail.com>
2015-11-05 08:20:58 -0500

    start work thursday

--- app.rb
+++ app.rb
@@ -29,6 +29,64 @@ apartments=[{id: 1, address: "70335 Clemenargaertine Tunnel", monthly_rent: 800,
  {id: 18, address: "8865 Borer Viaduct", monthly_rent: 600, sqft: 600, num_beds: 1, num_baths: 4},
  {id: 19, address: "7357 Emard Row", monthly_rent: 2600, sqft: 2300, num_beds: 3, num_baths: 2}]
 
+tenants=[{id: 1, name: "Maudie Mosciski", age: 90, gender: "Female", apartment_id: 1},
+  {id: 2, name: "Kristin Wisoky", age: 23, gender: "Female", apartment_id: 1},
+  {id: 3, name: "Demario King", age: 71, gender: "Female", apartment_id: 2},
+  {id: 4, name: "Kaitlin Cormier", age: 91, gender: "Male", apartment_id: 3},
+  {id: 5, name: "Rosalinda Champlin", age: 39, gender: "Male", apartment_id: 3},
+  {id: 6, name: "Valentin Keebler Sr.", age: 74, gender: "Male", apartment_id: 5},
+  {id: 7, name: "Ms. Garland Beatty", age: 60, gender: "Female", apartment_id: 6},
+  {id: 8, name: "Eryn Lynch", age: 12, gender: "Male", apartment_id: 6},
+  {id: 9, name: "Tiara Conn", age: 3, gender: "Male", apartment_id: 7},
+  {id: 10, name: "Tillman Schroeder", age: 27, gender: "Male", apartment_id: 7},
+  {id: 11, name: "Amber Jewess", age: 57, gender: "Female", apartment_id: 7},
+  {id: 12, name: "America Pollich", age: 82, gender: "Female", apartment_id: 7},
+  {id: 13, name: "Mylene Krajcik", age: 25, gender: "Male", apartment_id: 7},
+  {id: 14, name: "Josh Gottlieb", age: 8, gender: "Female", apartment_id: 8},
+  {id: 15, name: "Sherwood Stiedemann", age: 72, gender: "Female", apartment_id: 8},
+  {id: 16, name: "Katarina Kunde", age: 6, gender: "Female", apartment_id: 8},
+  {id: 17, name: "Mabelle Eichmann", age: 16, gender: "Male", apartment_id: 9},
+  {id: 18, name: "Mason Blanda", age: 83, gender: "Female", apartment_id: 9},
+  {id: 19, name: "Sonny Dibbert I", age: 30, gender: "Female", apartment_id: 9},
+  {id: 20, name: "Gus Herman II", age: 65, gender: "Male", apartment_id: 9},
+  {id: 21, name: "Delia Christiansen MD", age: 57, gender: "Female", apartment_id: 9},
+  {id: 22, name: "Willow Ledner Jr.", age: 46, gender: "Male", apartment_id: 9},
+  {id: 23, name: "Titus Harvey", age: 19, gender: "Male", apartment_id: 10},
+  {id: 24, name: "Ms. Randal Konopelski", age: 7, gender: "Female", apartment_id: 10},
+  {id: 25, name: "Caleb Maggio", age: 55, gender: "Male", apartment_id: 11},
+  {id: 26, name: "Hilbert Effertz", age: 30, gender: "Male", apartment_id: 11},
+  {id: 27, name: "Bartholome Herman", age: 93, gender: "Male", apartment_id: 11},
+  {id: 28, name: "Merritt Durgan", age: 34, gender: "Female", apartment_id: 11},
+  {id: 29, name: "Helmer Grimes", age: 80, gender: "Female", apartment_id: 14},
+  {id: 30, name: "Derrick Farrell", age: 81, gender: "Male", apartment_id: 14},
+  {id: 31, name: "Enola Sauer V", age: 31, gender: "Female", apartment_id: 14},
+  {id: 32, name: "Jimmy Bartell", age: 22, gender: "Female", apartment_id: 14},
+  {id: 33, name: "Christophe Boyle DVM", age: 55, gender: "Female", apartment_id: 15},
+  {id: 34, name: "Miss Wanda Kris", age: 57, gender: "Female", apartment_id: 15},
+  {id: 35, name: "Miss Maeve Goldner", age: 10, gender: "Male", apartment_id: 15},
+  {id: 36, name: "Noemie Daniel", age: 15, gender: "Female", apartment_id: 15},
+  {id: 37, name: "Shanna Jacobson", age: 38, gender: "Female", apartment_id: 15},
+  {id: 38, name: "Javier Boehm", age: 82, gender: "Female", apartment_id: 15},
+  {id: 39, name: "Aurelia Harvey", age: 35, gender: "Male", apartment_id: 16},
+  {id: 40, name: "Dangelo Bogisich", age: 60, gender: "Female", apartment_id: 16},
+  {id: 41, name: "Benton Abernathy", age: 55, gender: "Female", apartment_id: 16},
+  {id: 42, name: "Marina Kemmer", age: 37, gender: "Female", apartment_id: 16},
+  {id: 43, name: "Dasia Vandervort", age: 4, gender: "Female", apartment_id: 16},
+  {id: 44, name: "Bo Medhurst", age: 6, gender: "Male", apartment_id: 17},
+  {id: 45, name: "Elta Fay", age: 6, gender: "Female", apartment_id: 18},
+  {id: 46, name: "Conner Dare", age: 92, gender: "Male", apartment_id: 18},
+  {id: 47, name: "Dr. Lindsey Reichert", age: 18, gender: "Male", apartment_id: 18},
+  {id: 48, name: "Carlee Nolan", age: 77, gender: "Female", apartment_id: 18},
+  {id: 49, name: "Bernie Weimann", age: 11, gender: "Male", apartment_id: 18},
+  {id: 50, name: "Jose Denesik", age: 26, gender: "Female", apartment_id: 18},
+  {id: 51, name: "Guadalupe OHara", age: 9, gender: "Female", apartment_id: 20},
+  {id: 52, name: "Sheridan Jacobson Sr.", age: 6, gender: "Female", apartment_id: 20},
+  {id: 53, name: "Bennett Jakubowski", age: 69, gender: "Female", apartment_id: 20},
+  {id: 54, name: "Annette Stamm", age: 76, gender: "Male", apartment_id: 20},
+  {id: 55, name: "Dr. Raquel Carter", age: 61, gender: "Female", apartment_id: 20},
+  {id: 56, name: "Grover Rath", age: 9, gender: "Female", apartment_id: 20}]
+
+
 get '/' do
   'mia is awesome!'
 end
@@ -39,11 +97,19 @@ get '/apartments' do
 end
 
 get '/apartments/new' do
-  erb :new_apartment
+  erb :apartments_new
 end
 
 get '/apartments/:id' do
   @id=params[:id].to_i-1
   @apartments=apartments
-  erb :apartment_id
+  erb :apartments_id
+end
+
+get '/apartments/:id/tenants' do
+  @id=params[:id].to_i-1
+  @apartments=apartments
+  @tenants=tenants
+  erb :apartments_id_tenants
+  binding.pry
 end
