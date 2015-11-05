75d558c5d7f5aa0dda3c5007019488458426c871
Chase Gruber <chasegruber@gmail.com>
2015-11-05 12:49:00 -0500

    setup workspace

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require "pg"
 require "active_record"
 require "pry"
+require "sinatra"
+require "sinatra/reloader"
 
 require_relative "db/connection"
 require_relative "models/apartments"
