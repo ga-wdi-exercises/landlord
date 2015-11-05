320116b3b7a83c24b3c38304e90d6ef226886031
Elise Kain <elise.kain@gmail.com>
2015-11-04 21:57:35 -0500

    Connect views with db

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ end
 
 # Show all apartments
 get "/apartments" do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
 
@@ -24,17 +25,48 @@ get "/apartments/new" do
   erb :"apartments/new"
 end
 
+# Post new apartment form
+post '/apartments' do
+  @new_apt = Apartment.create(address: params[:address], monthly_rent: params[:monthly_rent], sqft: params[:sqft], num_beds: params[:num_beds], num_baths: params[:num_beds])
+  redirect '/apartments/' + @new_apt.id.to_s
+end
+
 # Show apartment details by id
 get "/apartments/:apt_id" do
+  @apartments = Apartment.all
+  @apt_id = params[:apt_id].to_i
+  @apt_details = Apartment.find(@apt_id)
   erb :"apartments/show"
 end
 
-# Show all tenants in apartment (by apt_id)
-get "/apartments/:apt_id/tenants" do
+# Edit apartment details
+
+# Delete apartment
+delete "/apartments/:apt_id" do
+  Apartment.destroy(params[:apt_id])
+  redirect "/apartments"
+end
+
+# Show all tenants
+get "/tenants" do
+  @tenants = Tenant.all
   erb :"tenants/index"
 end
 
-# Show all tenants in apartment (by apt_id)
-get "/apartments/:apt_id/tenants/new" do
+# Add Tenant
+get "/tenants/new" do
   erb :"tenants/new"
+end
+
+# Post new tenant form
+post '/tenants' do
+  @new_tenant = Tenant.create(name: params[:name], age: params[:age], gender: params[:gender])
+  redirect '/tenants/' + @new_tenant.id.to_s
+end
+
+get "/tenants/:tenant_id" do
+  @tenants = Tenant.all
+  @tenant_id = params[:tenant_id].to_i
+  @tenant_details = Tenant.find(@tenant_id)
+  erb :"tenants/show"
 end
