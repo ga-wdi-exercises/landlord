ce71c8ee9a9225da2f59e1a8d9eb094da74668f3
Starr Z. Chen <chenstarr@gmail.com>
2015-11-06 00:08:24 -0500

    Cleaned up models

--- models/apartment.rb
+++ models/apartment.rb
@@ -1,19 +1,3 @@
 class Apartment < ActiveRecord::Base
   has_many :tenants
 end
-
-  # def initialize address, monthly_rent, sqft, num_beds, num_baths
-  #   @address = address.to_s
-  #   @monthly_rent = monthly_rent
-  #   @sqft = sqft
-  #   @num_beds = num_beds
-  #   @num_baths = num_baths
-  #   @renters = []
-  # end
-  #
-  # def add_tenant tenant
-  #   if @renters.length < @num_beds
-  #     @renters << tenant
-  #   else puts "You're over capacity"
-  #   end
-  # end
--- models/tenant.rb
+++ models/tenant.rb
@@ -1,10 +1,3 @@
 class Tenant < ActiveRecord::Base
   belongs_to :apartment
 end
-
-  #
-  # def initialize name, age, gender
-  #   @name = name
-  #   @age = age
-  #   @gender = gender
-  # end
