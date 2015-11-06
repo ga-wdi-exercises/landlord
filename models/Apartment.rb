8760203c73c5b97e999ae2eacd9a5772bd235508
Rachel Porter <rachelandrewsporter@gmail.com>
2015-11-05 21:30:40 -0500

    organizes views and updates styles

--- models/Apartment.rb
+++ models/Apartment.rb
@@ -1,14 +1,3 @@
 class Apartment < ActiveRecord::Base
   has_many :tenants
-
-  # def add_tenant new_tenant
-  #
-  #   if renters.count < num_beds
-  #     renters << new_tenant
-  #   else
-  #   puts "Sorry. This apartment is full."
-  #   end #if-else closure
-  #
-  # end #add_tenant closure
-
-end #class closure
+end
