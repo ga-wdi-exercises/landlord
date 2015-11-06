b6ceb61e63fc86c70ecefa92193826853505cada
jen pengelly <jgpengelly@gmail.com>
2015-11-05 22:32:38 -0500

    Updated controller and erb files

--- controllers/apartments_controller.rb
+++ controllers/apartments_controller.rb
@@ -8,25 +8,25 @@ end
 # Route will show a form for adding a new apartment
 # Form includes address, monthly_rent, sqft, num_beds, num_baths, renters
 # this has to come before id
-# get '/apartments/new' do
-#   erb :"apartments/new"
-# end
-#
+get '/apartments/new' do
+  erb :"apartments/new"
+end
+
 # # View a single apartment's details
-# get '/apartments/:id' do
-#   @apartment = Apartment.find(params[:id])
-#   erb :"apartments/show"
-# end
-#
+get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/show"
+end
+
 # # Create a new tenant (in apartment 1?)
 # # Route will show a form for adding a new tenant
 # # Form includes Name, Age, Gender
 # get '/apartments/:id/tenants/new' do
+#
 #   erb :"tenants/new"
 # end
 #
-#
-# # List all tenants for apartment 1
+# # List all tenants for apartment
 # get '/apartments/:id/tenants' do
 #   erb :"tenants/index"
 # end
