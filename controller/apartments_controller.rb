44768b16fd7b07d25c0f957d02abf4d74de42acd
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-11-05 23:49:45 -0500

    views, controllers set up. Apartments and tenants iterated

--- controller/apartments_controller.rb
+++ controller/apartments_controller.rb
@@ -2,7 +2,26 @@ get '/' do
   erb :home
 end
 
+get '/' do
+  @address = params[:address]
+  @monthly_rent = params[:monthly_rent]
+  @sqft = params[:sqft]
+  @num_beds = params[:num_beds]
+  @num_baths = params[:num_baths]
+  erb :"apartments/index"
+end
+
+post '/apartments' do
+  Apartment.create! (params[:apartment])
+  redirect "/apartments"
+end
+
 get '/apartments' do
   @apartments = Apartment.all
   erb :"apartments/index"
+end
+
+get '/apartments/:id' do
+  @apartments = Apartment.find(params[:id])
+  erb :"apartments/show"
 end
