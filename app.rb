e3b0ce0ee174a56ca4cf07d8fd3651f0e121eb1d
Thom Householder <thomhouseholder@gmail.com>
2015-11-05 21:41:38 -0500

    created index, views, public, added sinatra gems

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require "pg" # postrgres db library
 require "active_record" # the ORM
 require "pry" # for debugging
 
+require "sinatra"
+require "sinatra/reloader"
+
 require_relative "db/connection" # require the db connection file that connects us to PSQL, prior to loading models
 require_relative "models/apartment" # require the Apartments class definition that we defined in the models/apartment.rb file
 require_relative "models/tenant" # require the Tenants class definition that we defined in the models/tenants.rb file
@@ -10,4 +13,14 @@ require_relative "models/tenant" # require the Tenants class definition that we 
 # This will put us into a state of the pry REPL, in which we've established a connection
 # with the wdi database and have defined the Apartment Class as an
 # ActiveRecord::Base class.
-binding.pry
+
+require_relative "apartments_controller.rb" #aparment routes will go in this file instead of app.rb
+require_relative "tenants_controller.rb" #tenant routes go here
+
+get '/' do
+  erb :index
+end
+
+get '/layout' do
+  erb :layout
+end
