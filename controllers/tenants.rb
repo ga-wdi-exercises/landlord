bdc8e2f612852ce6bc050261c53e34e0b4f4b94a
Jared Nichols <jnichols300@gmail.com>
2015-11-06 00:01:05 -0500

    first commit

---
+++ controllers/tenants.rb
@@ -0,0 +1,17 @@
+get '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
+  erb :"tenants/index"
+end
+
+# new
+get '/apartments/:id/tenants/new' do
+  @apartment = Apartment.find(params[:id])
+  erb :"tenants/new"
+end
+
+# create
+post '/apartments/:id/tenants' do
+  apartment = Apartment.find(params[:id])
+  apartment.tenants.create!(params[:apartment])
+  redirect "/apartments/#{apartment.id}/tenants"
+end
--- db/app.rb
+++ db/app.rb
@@ -1,26 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require "bundler/setup"
-require "pg"
-require "active_record"
-require "pry"
 
-require_relative "db/connection"
-require_relative "models/apartment"
-require_relative "models/tenant"
+require_relative 'db_connection'
+require_relative 'models/apartment'
+require_relative 'models/tenant'
+require_relative 'controllers/welcome'
+require_relative 'controllers/apartments'
+require_relative 'controllers/tenants'
 
-get '/' do
-  erb:":apartments"
-end
-
-get '/apartments' do
-  erb:"apartments/1"
-end
-
-get '/apartments/new' do
-  erb:"apartments/new"
-end
-
-get '/apartments/1/tenants' do
-  erb:"apartments/1/tenants"
-end
+# get '/' do
+#   erb:"apartments/show"
+# end
+#
+# get '/apartments' do
+#   erb:"apartments/:id"
+# end
+#
+# get '/apartments/new' do
+#   erb:"apartments/new"
+# end
+#
+# get '/apartments/:id/tenants' do
+#   erb:"apartments/:id/tenants"
+# end
