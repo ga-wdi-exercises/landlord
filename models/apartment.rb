40fd663beb9eea14c4c1e6d79dae0f55e0fbefdc
ckib16 <ckib16@gmail.com>
2015-11-05 20:56:37 -0500

    Fix the seed.rb process
    Copied to speed up Sinatra HW flow

--- models/apartment.rb
+++ models/apartment.rb
@@ -6,14 +6,14 @@ class Apartment < ActiveRecord::Base
 
   attr_accessor :address, :monthly_rent, :sqft, :num_beds, :num_baths, :renters
 
-  def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
-    @address = address
-    @monthly_rent = monthly_rent
-    @sqft = sqft
-    @num_beds = num_beds
-    @num_baths = num_baths
-    @renters = renters
-  end
+  # def initialize(address, monthly_rent, sqft, num_beds, num_baths, renters)
+  #   @address = address
+  #   @monthly_rent = monthly_rent
+  #   @sqft = sqft
+  #   @num_beds = num_beds
+  #   @num_baths = num_baths
+  #   @renters = renters
+  # end
 
   def add_tenant(new_tenants)
     if renters < num_beds
@@ -24,5 +24,5 @@ class Apartment < ActiveRecord::Base
   end
 end
 
-new_apartment = Apartment.new('1002 Bell Street', 3000, 2000, 3, 3, 1)
-new_apartment.add_tenant(2)
+# new_apartment = Apartment.new('1002 Bell Street', 3000, 2000, 3, 3, 1)
+# new_apartment.add_tenant(2)
