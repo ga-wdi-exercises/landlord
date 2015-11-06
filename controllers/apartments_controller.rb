eb971e9bd81835e49851e012eee7079015cfb44e
Samir Mehta <sammehta88@gmail.com>
2015-11-06 00:29:11 -0500

    Add put and delete methods

--- controllers/apartments_controller.rb
+++ controllers/apartments_controller.rb
@@ -13,6 +13,11 @@ get "/apartments/new" do
   erb :"apartments/new"
 end
 
+get "/apartment/:id/edit" do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/edit"
+end
+
 post "/apartments" do
   @addess = params[:address]
   Apartment.create(params.except("_method"))
@@ -24,4 +29,14 @@ delete "/apartment/:id" do
   @tenants = @apartment.tenants
   @apartment.destroy
   redirect "/apartments"
+end
+
+# why does Apartment.all array have edited aparments at the end of the array
+# even though id has not changed?
+put "/apartment/:id" do
+  @apartment = Apartment.find(params[:id])
+  # why does below code require exception of splat and captures but create
+  # method above does not?
+  @apartment.update(params.except("_method", "splat", "captures", "id"))
+  redirect "/apartment/#{@apartment.id}"
 end
