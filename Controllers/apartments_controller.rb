9ef7a3b5bc6a7adcb2de22241d2933e8b3bf87b2
Ly Nguyen <Ly900@aol.com>
2015-11-05 22:37:06 -0500

    link to add apartments erb with form; form not working yet

--- Controllers/apartments_controller.rb
+++ Controllers/apartments_controller.rb
@@ -3,6 +3,10 @@ get "/apartments" do
   erb :"apartments/apartments_index"
 end
 
+get "/apartments/new" do
+  erb :"apartments/apartments_new"
+end
+
 get "/apartments/:id" do
   @apartment = Apartment.find(params[:id].to_i)
   erb :"apartments/apartments_details"
