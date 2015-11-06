513a4b39054fb693c2d1359125ccddaddcca2aba
Thom Householder <thomhouseholder@gmail.com>
2015-11-05 22:22:05 -0500

    created add new tenant

--- tenants_controller.rb
+++ tenants_controller.rb
@@ -4,3 +4,14 @@ get "/tenants" do
   @tenants = Tenant.all
   erb :"tenants/index"
 end
+
+# not sure yet what i'm going to do with this page since form lives on layout
+get "/new" do
+  erb :"tenants/new"
+end
+
+# this listens for creation in form and posts to tenants page
+post '/tenants' do
+  Tenant.create(name: params[:new_tenant], age: params[:new_tenant_age] , gender: params[:new_tenant_gender])
+  redirect "/tenants"
+end
