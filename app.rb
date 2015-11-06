692b4bbdab680f60e54922770f065055df8227bf
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-11-05 22:38:41 -0500

    added sinatra db

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require "sinatra"
+require "sinatra/reloader"
 require "bundler/setup" # require all the gems we'll be using for this app from the Gemfile. Obviates the need for `bundle exec`
 require "pg" # postrgres db library
 require "active_record" # the ORM
@@ -6,8 +8,13 @@ require "pry" # for debugging
 require_relative "db/connection" # require the db connection file that connects us to PSQL, prior to loading models
 require_relative "models/apartment" # require the Student class definition that we defined in the models/student.rb file
 require_relative "models/tenant"
-require_relative "db/seeds.rb"
+require_relative "controllers/apartments_controller.rb"
+require_relative "controllers/tenants_controller.rb"
+# require_relative "db/seeds.rb"
 # This will put us into a state of the pry REPL, in which we've established a connection
 # with the wdi database and have defined the Student Class as an
 # ActiveRecord::Base class.
-binding.pry
+# binding.pry
+get '/' do
+  erb :home
+end
