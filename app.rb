0390245792ed29a49e5da476e342c480b18ce71e
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-11-06 08:54:47 -0500

    Add Sinatra

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require "bundler/setup"
 require "pg"
 require "active_record"
+require 'sinatra'
+require 'sinatra/reloader'
 require "pry"
 
 
 require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
+require_relative "controllers/apartments"
 
-
-binding.pry
+get '/' do
+  erb :home
+end
