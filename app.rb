347d9e6678ae86a6d227eb8fd9939dddc9687827
Sarah Hersh <schersh@gmail.com>
2015-11-05 20:39:41 -0500

    added tenant and apt get paths

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
+# required gem files
 require "bundler/setup"
 require "pg"
 require "active_record"
 require "pry"
-
+require 'sinatra'
+require 'sinatra/reloader'
+# required relative files
 require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
+require_relative "controllers/apartments"
+require_relative "controllers/tenants"
 
-binding.pry
+# general routes for sinatra
+get '/' do
+  erb :home
+end
