4eab5adeb9d57121ad6ab6ad15c927e148f5cf43
Spicy Legato <jpqreichard@gmail.com>
2016-04-12 18:28:53 -0400

    maybe deletes whitespace... not sure: bumped into something

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -5,6 +5,7 @@ require "pg" # postgres db library
 require "active_record" # the ORM
 require "pry" # for debugging
 
+
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
   :database => "landlord"
