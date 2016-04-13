4818c5aafd4616d7e171f5ae11ca6132f582c0d8
Zaneh Williams <zanehmw@gmail.com>
2016-04-13 05:29:58 -0400

    working on step three- almost done

--- console.rb
+++ console.rb
@@ -17,6 +17,33 @@ puts "*" * 50
 puts "The first apartment is at #{Apartment.first.address}."
 puts "It has  #{Apartment.first.tenants.count} tenants."
 
-binding.pry
+# binding.pry
+#
+# puts "ignore this line, it's just here so the binding.pry above works"
 
-puts "ignore this line, it's just here so the binding.pry above works"
+
+# ruby_apartments = apartments.map do |apartment|
+#   Apartment.new(apartment[:id], apartment[:address], apartment[:monthly_rent], apartment[:square_feet])
+# end
+#
+# ruby_tenants = tenants.map do |tenant|
+#   Tenant.new(tenant[:id], tenant[:name], tenant[:age], tenant[:apartment_id])
+# end
+puts "*" * 50
+puts "Hello welcome to this app."
+puts "Type '1' to view all of your apartments"
+puts "Type '2' to view all of your tenants"
+puts "Type '3' to view all of your tenants"
+track_number = gets.chomp
+if track_number == "1"
+  puts apartments
+elsif track_number == "2"
+  puts tenants
+elsif track_number == "3"
+  #trying to figure out the code that goes here
+else
+  puts "Hello welcome to this app."
+  puts "Type '1' to view all of your apartments"
+  puts "Type '2' to view all of your tenants"
+  puts "Type '3' to view all of your tenants"
+end
