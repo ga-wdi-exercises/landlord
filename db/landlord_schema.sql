b8eea62c89e3980876a33a82488f312c02c3dbbf
Louis <LOHankins@gmail.com>
2016-04-13 08:55:20 -0400

    getting version in before 9am deadline.  still needs work.  create record now works.

--- db/landlord_schema.sql
+++ db/landlord_schema.sql
@@ -4,17 +4,17 @@ DROP TABLE IF EXISTS apartments;
 CREATE TABLE tenants (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  age TEXT,
-  gender TEXT,
+  age TEXT NOT NULL,
+  gender VARCHAR,
   apartment_id INT
 );
 
 CREATE TABLE apartments (
   id SERIAL PRIMARY KEY,
   address TEXT NOT NULL,
-  monthly_rent TEXT NOT NULL,
-  sqft TEXT,
-  num_beds TEXT,
-  num_baths TEXT
+  monthly_rent INT,
+  sqft INT,
+  num_beds INT,
+  num_baths INT
 );
 
