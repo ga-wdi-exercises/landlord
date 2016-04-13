b92fdb4c29da6e2135c16dfee6ac69c8d420a9a5
Matt <mvaden08@gmail.com>
2016-04-12 20:46:51 -0400

    More seedlings. Destroy all, apartments, tenants, comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@ require_relative "models/tenant"
 Tenant.destroy_all
 Apartment.destroy_all
 
-
+# Apartments
 the_bat_cave = Apartment.create(address: "123 Main St", monthly_rent: 2000, sqft: 600, num_beds: 2, num_baths: 1)
 the_sinatra = Apartment.create(address: "1094 Bergen St", monthly_rent: 1800, sqft: 700, num_beds: 2, num_baths: 1)
 the_wu_tang = Apartment.create(address: "2970 Flatbush Ave", monthly_rent: 1100, sqft: 650, num_beds: 1.5, num_baths: 1)
 the_jeter = Apartment.create(address: "825 Fulton St", monthly_rent: 1300, sqft: 480, num_beds: 1, num_baths: 1)
 
+# Tenants
 margaret = Tenant.create(name: "Margaret", age: 24, gender: "Female", apartment_id: 22)
 adam = Tenant.create(name: "Adam", age: 30, gender: "Male", apartment_id: 24)
 benedict = Tenant.create(name: "Benedict", age: 85, gender: "Male", apartment_id: 22)
