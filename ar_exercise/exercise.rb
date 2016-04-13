fab2b7ca40498fffe6a43759fbecff6c90c8a5fc
Matt <mvaden08@gmail.com>
2016-04-13 00:48:38 -0400

    more exercising

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -40,23 +40,35 @@ all_tenants = Tenant.all
 
 # get the first tenant in the DB
 all_tenants = Tenant.first
+
 # get all tenants older than 65
 all_tenants = Tenant.where("age: > ?" "65")
 
 # get all apartments whose price is greater than $2300
+all_apartments = Apartment.where("age: > ?" "2300")
+
 # get the apartment with the address "6005 Damien Corners"
+all_apartments = Apartment.where(address:"6005 Damien Corners")
+
 # get all tenants in that apartment
+all_tenants = Tenant.where(apartment_id: 6)
 
 # Use `each` and `puts` to:
 # Display the name and ID # of every tenant
+Tenant.all.each do |tenant|
+  puts "Name: #{tenant.name}, Id: #{tenant.id}"
+end
 # Iterate over each apartment, for each apartment, display it's address and rent price
+Apartment.all.each do |apartment|
+  puts "Address: #{apartment.address}, Rent: $#{apartment.monthly_rent}"
+end
 # Iterate over each apartment, for each apartment, display it's address and all of it's tenants
 
 ################################################
 # CREATING / UPDATING / DELETING
 ################################################
 
-# Hint, the following methods will help: `new`, `create`, `save`, `uddate`, `destroy`
+# Hint, the following methods will help: `new`, `create`, `save`, `update`, `destroy`
 
 # Create 3 new apartments, and save them to the DB
 the_bat_cave = Apartment.create(address: "123 Main St", monthly_rent: 2000, sqft: 600, num_beds: 2, num_baths: 1)
@@ -83,10 +95,12 @@ cassandra = Tenant.create(name: "Cassandra", age: 26, gender: "Female", apartmen
 # Birthday!
 # It's Kristin Wisoky's birthday. Find her in the DB and change her age to be 1 year older
 # Note: She's in the seed data, so she should be in your DB
+Tenant.find_by(name: "Kristin Wisoky").increment(:age)
 
 # Rennovation!
 # Find the apartment "62897 Verna Walk" and update it to have an additional bedroom
 # Make sure to save the results to your database
+apartment = Apartment.where(address:"62897 Verna Walk")
 
 # Rent Adjustment!
 # Update the same apartment that you just 'rennovated'. Increase it's rent by $400
