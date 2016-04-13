9b016f107e1d1f9736beda5abfd6ffdae183c156
Mary Griffus <mary.e.shultz@gmail.com>
2016-04-13 08:59:24 -0400

    added apartment lookup from tenant name

--- console.rb
+++ console.rb
@@ -14,56 +14,78 @@ require_relative "models/tenant"
 
 # binding.pry
 
-2.times {puts "*" * 70}
-puts "Welcome to Landlord"
-puts "Options:"
-puts "1) See all apartments"
-puts "2) See all tenants"
-puts "3) See all apartments and associated tenants"
-2.times {puts "*" * 70}
-puts ""
-puts "Please enter your selection"
+choice = nil
+while choice != 'q'
 
-input = gets.chomp.to_i
+  2.times {puts "*" * 70}
+  puts "Welcome to Landlord
+        Options:
+        1) See all apartments
+        2) See all tenants
+        3) See all apartments and associated tenants
+        4) Look up apartment from tenant name
+        q) Exit the program"
+  2.times {puts "*" * 70}
+  puts ""
+  puts "Please enter your selection"
 
-case input
-when 1
-  puts 'All apartments'
-  puts 'ID    | Address                  | Rent   | Sq Feet  | Beds | Baths'
-  puts '*' * 70
-  all_apartments = Apartment.all
-  all_apartments.each do |apartment|
-    print apartment.id, (' ' * (6-apartment.id.to_s.length)), '| ',
-      apartment.address, (' ' * (25-apartment.address.to_s.length)), '| ',
-      apartment.monthly_rent, (' ' * (7-apartment.monthly_rent.to_s.length)), '| ',
-      apartment.sqft, (' ' * (9-apartment.sqft.to_s.length)), '| ',
-      apartment.num_beds, (' ' * (5-apartment.num_beds.to_s.length)), '| ',
-      apartment.num_baths
-    puts ''
+  choice = nil
+  while ![1, 2, 3, 4, 'q'].include? choice
+    choice = (gets.chomp)
+    choice = choice.to_i unless choice == 'q'
   end
 
-when 2
-  puts 'All tenants'
-  puts 'ID    | Name                   | Gender         | Age   | Apartment ID'
-  puts '*' * 70
-  all_tenants = Tenant.all
-  all_tenants.each do |tenant|
-    print tenant.id, (' ' * (6-tenant.id.to_s.length)), '| ',
-      tenant.name, (' ' * (23-tenant.name.to_s.length)), '| ',
-      tenant.gender, (' ' * (15-tenant.gender.to_s.length)), '| ',
-      tenant.age, (' ' * (6-tenant.age.to_s.length)), '| ',
-      tenant.apartment_id
-    puts ''
-  end
+  case choice
+  when 1
+    puts 'All apartments'
+    puts 'ID    | Address                  | Rent   | Sq Feet  | Beds | Baths'
+    puts '*' * 70
+    all_apartments = Apartment.all
+    all_apartments.each do |apartment|
+      print apartment.id, (' ' * (6-apartment.id.to_s.length)), '| ',
+        apartment.address, (' ' * (25-apartment.address.to_s.length)), '| ',
+        apartment.monthly_rent, (' ' * (7-apartment.monthly_rent.to_s.length)), '| ',
+        apartment.sqft, (' ' * (9-apartment.sqft.to_s.length)), '| ',
+        apartment.num_beds, (' ' * (5-apartment.num_beds.to_s.length)), '| ',
+        apartment.num_baths
+      puts ''
+    end
 
-when 3
-  puts 'Apartments listed with associated tenants'
-  all_apartments = Apartment.all
-  all_tenants = Tenant.all
-  all_apartments.each do |apartment|
-    print apartment.address
-    puts ''
-    tenants_in_apartment = all_tenants.select {|tenant| tenant.apartment_id == apartment.id}
-    tenants_in_apartment.each {|person| print '  '; puts person.name}
+  when 2
+    puts 'All tenants'
+    puts 'ID    | Name                   | Gender         | Age   | Apartment ID'
+    puts '*' * 70
+    all_tenants = Tenant.all
+    all_tenants.each do |tenant|
+      print tenant.id, (' ' * (6-tenant.id.to_s.length)), '| ',
+        tenant.name, (' ' * (23-tenant.name.to_s.length)), '| ',
+        tenant.gender, (' ' * (15-tenant.gender.to_s.length)), '| ',
+        tenant.age, (' ' * (6-tenant.age.to_s.length)), '| ',
+        tenant.apartment_id
+      puts ''
+    end
+
+  when 3
+    puts 'Apartments listed with associated tenants'
+    all_apartments = Apartment.all
+    all_tenants = Tenant.all
+    all_apartments.each do |apartment|
+      print apartment.address
+      puts ''
+      tenants_in_apartment = all_tenants.select {|tenant| tenant.apartment_id == apartment.id}
+      tenants_in_apartment.each {|person| print '  '; puts person.name}
+    end
+
+  when 4
+    puts "What is the tenant's name?"
+    raw_lookup = gets.chomp
+    tenant_lookup = raw_lookup.split.map(&:capitalize).join(' ')
+    apt_lookup = nil
+    all_tenants = Tenant.all
+    this_tenant = all_tenants.find {|tenant| tenant.name == tenant_lookup}
+    apt_lookup = this_tenant.apartment_id
+    all_apartments = Apartment.all
+    this_apartment = all_apartments.find {|apartment| apartment.id == apt_lookup}
+    puts "#{tenant_lookup} is renting #{this_apartment.address}"
   end
 end
