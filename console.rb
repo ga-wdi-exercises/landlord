6fd7e7ce556d4e81eee0e7d0bb7e1d887b10c782
Habby Whyte <bahybaj@hotmail.com>
2016-04-13 00:37:11 -0400

    completed command line lanlord app- bronze

--- console.rb
+++ console.rb
@@ -7,16 +7,56 @@ require_relative "models/apartment"
 require_relative "models/tenant"
 
 
+
+
 # NOTE: Uncomment the lines below to verify that your seed script is working
 
-puts "There are #{Apartment.count} apartments"
-puts "There are #{Tenant.count} tenants"
-
+# puts "There are #{Apartment.count} apartments"
+# puts "There are #{Tenant.count} tenants"
+#
+# puts "*" * 50
+#
+# puts "The first apartment is at #{Apartment.first.address}."
+# puts "It has  #{Apartment.first.tenants.count} tenants."
+#
+# binding.pry
 puts "*" * 50
 
-puts "The first apartment is at #{Apartment.first.address}."
-puts "It has  #{Apartment.first.tenants.count} tenants."
 
-binding.pry
+  puts "Please pick an option:"
+  puts "type '1' view all apartments;
+  type '2' view all tenants
+  type '3' view all tenants at each apartment"
+  option = gets.chomp
 
-# puts "ignore this line, it's just here so the binding.pry above works"
+  while option != "1" && option !="2"  && option !='3' && option !='4' do
+    puts "type '1' view all apartments;
+    type '2' view all tenants
+    type '3' view all tenants at each apartment"
+    option = gets.chomp
+    puts option
+  end
+
+  # option1 all apartments to include id/address/monthly_rent
+ if option == "1"
+   Apartment.find_each do |apartment|
+     puts " Apartment id: #{apartment.id}; address: #{apartment.address}; rent: #{apartment.monthly_rent}"
+   end
+
+  # option2 all tenants to include name/age
+elsif option == "2"
+  Tenant.find_each do |tenant|
+    puts "Tenant name: #{tenant.name}; age: #{tenant.age}"
+ end
+ #
+ # # option3 all apts and tenants/address/name
+elsif option == "3"
+  Apartment.find_each do |apartment|
+    puts "Apartment Address: #{apartment.address}"
+    apartment.tenants.find_each do |tenant|
+    puts "tenant: #{tenant.name}"
+  end
+ end
+
+
+end
