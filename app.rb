8d1dd0bca4bce18606751625788dc474f50e10e6
Matt <mvaden08@gmail.com>
2016-04-14 17:08:54 -0400

    massive updates to EVERYTHING.

--- app.rb
+++ app.rb
@@ -1,21 +1,44 @@
+require "sinatra/reloader"
 require "sinatra"
 require "active_record"
+
 # look for a gem so I Can use that in this file
 require_relative "models/apartment"
 require_relative "models/tenant"
-require_relative "dbconnection"
+require_relative "db/connection"
 # look for a file nearby
 
-# get "/" do
-#   @pizza = "I Love Pizza"
-#   #or
-#   erb :index
-#   '<h1>This is the root</h1><a href="/apartments"> go to apartments</a>'
-# end
+# address = []
 
-get "/apartment" do
-  @apartment = apartment.each 
-  # @pizza = "hello world apartments"
-  # @pizza = Apartment.all
+get "/apartments" do
+  @apartments = Apartment.all
   erb :"apartments/index"
 end
+
+# create apartments
+post '/apartments' do
+  @apartment = Apartment.create(address: params[:address], monthly_rent: params[:monthly_rent], sqft: params[:sqft], num_beds: params[:num_beds], num_baths: params[:num_baths])
+  redirect "/apartments/#{@apartment.id}"
+end
+
+# delete apartments
+delete '/apartments/:address' do
+  @apartments = Apartment.find(params[:address])
+  @apartments.destroy
+  redirect("/apartments")
+end
+
+# edit apartments
+get "/apartments/:address/edit" do
+  @apartments = Apartment.find(params[:address])
+  erb(:"apartmentss/edit")
+end
+
+put '/apartments/:address' do
+  @apartments = Apartment.find(params[:address])
+  @apartments.update(params[:apartments])
+  redirect("/apartments/#{@apartments.address}")
+end
+
+# show all tenants living in apartments
+# put '/apartments/:'
--- console.rb
+++ console.rb
@@ -15,3 +15,52 @@ require_relative "models/tenant"
 binding.pry
 
 puts "ignore this line, it's just here so the binding.pry above works"
+
+# Step 3 solution 
+
+def menu
+  puts "-" * 60
+  puts "Choose an option:
+  1) List all apartments
+  2) List all tenants
+  3) list all apartments and their associated tenants
+  q) Quit"
+  puts "-" * 60
+  return gets.chomp
+end
+
+system "clear"
+
+loop do
+  choice = menu
+
+  case choice
+  when "1"
+    system "clear"
+
+    Apartment.all.each do |apartment|
+      puts "#{apartment.id} | $#{apartment.monthly_rent} - #{apartment.address}"
+    end
+
+  when "2"
+    system "clear"
+
+    Tenant.all.each do |tenant|
+      puts "#{tenant.name} | age: #{tenant.age}"
+    end
+
+  when "3"
+    system "clear"
+
+    Apartment.all.each do |apartment|
+      puts "-" * 30
+      puts apartment.address
+      apartment.tenants.each do |tenant|
+        puts tenant.name
+      end
+    end
+
+  when "q"
+    break
+  end
+end
