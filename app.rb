c1917356f5ea7131158932b6a794adbd799477e2
Leland Jordon <lelandjordon@mac.com>
2016-04-14 18:51:15 -0400

    Building app.rb to make it like Tunr's app.rb.

--- app.rb
+++ app.rb
@@ -16,3 +16,43 @@ get '/apartments' do
   @apartments = Apartment.all
   erb :'/apartments/index'
 end
+
+get '/apartments/new' do
+  erb :'/apartments/new'
+end
+
+get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :"/apartments/show"
+end
+
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
+
+get '/tenants' do
+  @tenants = Song.all
+  erb :'/tenants/index'
+end
+
+get '/tenants/new' do
+  erb :'/tenants/new'
+end
+
+get '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  erb :'/tenants/show'
+end
+
+post '/tenants' do
+  @tenant = Tenant.create(params[:tenant])
+  redirect "/tenants/#{@song.id}"
+end
+
+delete '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  @tenant.destroy
+  redirect("/tenants")
+end
