33e08a74bde1c0fa2088b8f2293e8484f7fc855c
Leland Jordon <lelandjordon@mac.com>
2016-04-14 10:39:57 -0400

    added some new stuff.  Haven't tested any of it.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem "pg"            # lets it run Postgres
 gem "activerecord"  #Active Record. duh.
 gem "pry"           # Can make it use Pry.
+gem "sinatra"
---
+++ app.rb
@@ -0,0 +1,18 @@
+require "sinatra"
+require "active_record"
+require "pry"
+
+require_relative "models/apartment"
+require_relative "models/tenant"
+require_relative "db/connection"
+
+get '/' do
+  "Home page"
+  erb :index
+end
+
+get '/apartments' do
+  "Hello Apartments"
+  @pizza = Apartment.all
+  erb :'/apartments/index'
+end
