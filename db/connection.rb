18ddbc5d7f88cb78b31ecc2b09cf3a5421c0a8e6
Jacqui Ahn <jsahn23@gmail.com>
2016-07-24 13:43:10 -0400

    creates connection.rb file to handle cxn between our app and database

---
+++ db/connection.rb
@@ -0,0 +1,13 @@
+require "pg" # postgres db library
+require "active_record" # the ORM
+require "pry" # for debugging
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => "landlord"
+)
+
+
+binding.pry
+
+puts "end"
