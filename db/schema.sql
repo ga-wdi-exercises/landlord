7ea1f52861521e2c0369b6fd6fa062290a47b457
pamelaolney <pjolney12@gmail.com>
2016-07-24 14:57:22 -0400

    modified the schema file so now Apartments are not NULL!!

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,16 @@ DROP TABLE IF EXISTS apartments;
 CREATE TABLE tenants (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  age INT NOT NULL,
-  gender TEXT NOT NULL,
-  apartment_id INT NOT NULL
+  age INT,
+  gender TEXT,
+  apartment_id INT
 );
 
 CREATE TABLE apartments (
   id SERIAL PRIMARY KEY,
   address TEXT NOT NULL,
-  monthly_rent INT NOT NULL,
-  sqft INT NOT NULL,
-  num_beds INT NOT NULL
+  monthly_rent INT,
+  sqft INT,
+  num_beds INT,
+  num_baths INT
 );
