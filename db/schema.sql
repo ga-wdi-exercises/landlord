d2488fd4f70f021223bf44bbd32f4e0b6d022875
Alexander P. Holland <alexanderpholland@Alexanders-MacBook-Air.local>
2016-07-24 11:57:34 -0400

    loaded schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
+
+CREATE TABLE tenants (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INT,
+  gender TEXT,
+  apartment_id INT
+);
+CREATE TABLE apartments (
+  id SERIAL PRIMARY KEY,
+  address TEXT,
+  monthly_rent INT,
+  sqft INT,
+  num_beds INT,
+  num_baths INT
+);
