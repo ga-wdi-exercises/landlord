6f62e024743589069fd9b22195844c48d6620226
Sarah K Shultis <skshultis@Sks-MacBook-Air.fios-router.home>
2016-07-24 21:15:21 -0400

    exercises.rb complete

--- console.rb
+++ console.rb
@@ -1,6 +1,45 @@
+require "pg" # postgres db library
+require "active_record" # the ORM
+require "pry" # for debugging
+
+
 require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
+
+# binding.pry
+
+print "Input 1 for apartment list, 2 for tenants. Enter 'quit' to exit.\n"
+
+landlord = gets.chomp
+
+while landlord != "quit"
+
+  if landlord == "1"
+    #shows all apartments
+    Apartment.all.each do |apt|
+      puts apt[:address]
+    end
+  elsif landlord == "2"
+
+    #shows all tenants
+    Tenant.all.each do |tenant|
+      puts tenant[:name]
+    end
+
+  else
+    puts "Please enter a 1 or 2 to view property or tenant information, or 'quit' to exit."
+
+    # break if landlord == "quit"
+  end
+    puts "Enter 1 or 2"
+    landlord = gets.chomp
+  end
+
+  # puts "press <enter> to continue"
+
+  # end
+  #
 
 # NOTE: Uncomment the lines below to verify that your seed script is working
 
@@ -12,6 +51,6 @@ require_relative "models/tenant"
 # puts "The first apartment is at #{Apartment.first.address}."
 # puts "It has  #{Apartment.first.tenants.count} tenants."
 
-binding.pry
+# binding.pry
 
 puts "ignore this line, it's just here so the binding.pry above works"
