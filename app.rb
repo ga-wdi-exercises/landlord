85f2dea06d90b3e2f8fde0071d4821371050b674
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-25 18:33:26 -0400

    finished show, index, and new for apartments

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'pg'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+# Load the file to connect to the DB
+require_relative 'db/connection'
+
+# Load models
+require_relative 'models/apartment'
+require_relative 'models/tenant'
+
+get '/' do
+  redirect '/apartment'
+end
+
+get '/apartment' do
+  @apartments = Apartment.all
+  erb :"apartment/index"
+end
+
+get '/apartment/new' do
+  erb :"apartment/new"
+end
+
+get '/apartment/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartment/show"
+end
+
+
+post '/apartment' do
+  @apartment = Apartment.create(params[:apartment])
+ redirect "/apartment/#{@apartment.id}"
+end
