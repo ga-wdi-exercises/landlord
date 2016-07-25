8e5e674e2e0503b269e321e1fe0517830ab810db
michaelmahony <michael.mahony@umbc.edu>
2016-07-25 11:21:25 -0400

    Defined models, created connection and seed files.

--- console.rb
+++ console.rb
@@ -1,16 +1,20 @@
+require "bundler/setup" # require all the gems we'll be using for this app from the Gemfile. Obviates the need for `bundle exec`
+require "pg" # postgres db library
+require "active_record" # the ORM
+
 require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
 
 # NOTE: Uncomment the lines below to verify that your seed script is working
 
-# puts "There are #{Apartment.count} apartments"
-# puts "There are #{Tenant.count} tenants"
-#
-# puts "*" * 50
-#
-# puts "The first apartment is at #{Apartment.first.address}."
-# puts "It has  #{Apartment.first.tenants.count} tenants."
+puts "There are #{Apartment.count} apartments"
+puts "There are #{Tenant.count} tenants"
+
+puts "*" * 50
+
+puts "The first apartment is at #{Apartment.first.address}."
+puts "It has  #{Apartment.first.tenants.count} tenants."
 
 binding.pry
 
