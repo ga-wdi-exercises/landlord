a3e2d26ae3e706ebc45f6121691b9ceaffde83c6
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-26 13:42:10 -0400

    got show pages working

--- app.rb
+++ app.rb
@@ -15,3 +15,8 @@ get '/apartments' do
   @apartments = Apartment.all
   erb :'apartments/index'
 end
+
+get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :'apartments/show'
+end
