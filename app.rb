c2d3ab15ce94c7953c2f1742bf1c67bce812585e
Marc Fournier <marc.fournier91@gmail.com>
2016-07-26 15:57:34 -0400

    Added edit and delete paths

--- app.rb
+++ app.rb
@@ -28,3 +28,20 @@ get '/apartments/:id' do
   @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
 end
+
+get '/apartments/:id/edit' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/edit"
+end
+
+put '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
+delete '/apartments/:id' do
+  apartment = Apartment.find(params[:id])
+  apartment.destroy
+  redirect '/apartments'
+end
