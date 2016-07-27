009dd1900a36c4ec32fd6c5e87b240369e944720
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-27 18:47:36 -0400

    Changed .each to .map in app.rb. Moving on from CLI to Sinatra.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative "db/connection"
 require_relative "models/apartment"
+require_relative "models/tenant"
 
 
 puts "Hello! Welcome to Property Manager CLI."
@@ -8,6 +9,28 @@ puts "Enter '2' to View Tenants"
 puts "Enter '3' to Apartments and Associated Tenants"
 user_input = gets.chomp
 
-if user_input == "1"
-  puts Apartment.all
+def all_apts
+  Apartment.all.map do |apartment|
+    puts "Apartment ID :#{apartment.id}, Address: #{apartment.address}, Monthly Rent: #{apartment.monthly_rent}"
+  end
 end
+
+def all_tenants
+  Tenant.all.map do |tenant|
+    puts "Name: #{tenant.name}, Age: #{tenant.age}"
+  end
+end
+
+# def tenant_apt
+#   Tenant.all.each do |occupy|
+#     puts ""
+
+if user_input == "1"
+  puts all_apts
+end
+
+if user_input == "2"
+  puts all_tenants
+end
+
+# if user_input == "3"
