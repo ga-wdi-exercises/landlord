969b9e80239333c91405e42a7410e8ed1c861020
Tim Foley <thetimfoley@gmail.com>
2016-10-03 13:58:02 -0400

    created schema file

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
+
+CREATE TABLE tenants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  gender VARCHAR(255),
+  apartment_id
+);
+
+CREATE TABLE apartments (
+  id SERIAL PRIMARY KEY,
+  address VARCHAR,
+  monthly_rent INTEGER,
+  sqft INTEGER,
+  num_beds INTEGER,
+  num_baths INTEGER
+);
