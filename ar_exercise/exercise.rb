4ecf12abcd5d65a32f68e48a5c77c610764dc279
Barrett Cope <barrettcope@gmail.com>
2016-10-03 19:29:53 -0400

    Stopping point

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -34,10 +34,15 @@ end
 all_tenants = Tenant.all
 
 # get the first tenant in the DB
+first_tenant = Tenant.find(1)
 # get all tenants older than 65
+old_tenants = Tenant.where()
 # get all apartments whose price is greater than $2300
+
 # get the apartment with the address "6005 Damien Corners"
+damien = Apartment.where(address: "6005 Damien Corners")
 # get all tenants in that apartment
+
 
 # Use `each` and `puts` to:
 # Display the name and ID # of every tenant
@@ -69,3 +74,6 @@ all_tenants = Tenant.all
 # Millenial Eviction!
 # Find all tenants who are under 30 years old
 # Delete their records from the DB
+
+binding.pry
+puts "done"
