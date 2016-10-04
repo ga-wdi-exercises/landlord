5e97b6a4a26db722e6eef10a947ccd7291a7ad6b
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-04 19:50:03 -0400

    added tenants

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -79,6 +79,21 @@ louis = Apartment.create(address: "1920 14th Street NW", monthly_rent: 2759, sqf
 
 # Create at least 9 new tenants and save them to the DB. (Make sure they belong to an apartment)
 # Note: you'll use this little bit of code as a `seeds.rb` file later on.
+harbor_point.tenants.create(name: "Ethan Davila", age: 15, gender: "Female")
+harbor_point.tenants.create(name: "John Davila", age: 56, gender: "Male")
+harbor_point.tenants.create(name: "Eliana Davila", age: 18, gender: "Female")
+the_envoy.tenants.create(name: "Mariana Lungu", age: 32, gender: "Female")
+the_envoy.tenants.create(name: "Jonathan Davila", age: 28, gender: "Male")
+the_envoy.tenants.create(name: "Victoria Davila", age:3, gender: "Male")
+louis.tenants.create(name: "Irina Popescu", age: 26, gender: "Female")
+louis.tenants.create(name: "Nicole Miller", age: 28, gender: "Female")
+louis.tenants.create(name: "Oleg Hmeli", age: 30, gender: "Male")
+
+
+
+
+
+
 
 # Birthday!
 # It's Kristin Wisoky's birthday. Find her in the DB and change her age to be 1 year older
