a5a2d4781c4a69c8af9d7ded07066c44660ccdd5
manish pradhan <pradhanmanish33@gmail.com>
2016-10-04 19:46:24 -0400

    commit

--- console.rb
+++ console.rb
@@ -18,6 +18,30 @@ puts "*" * 50
 puts "The first apartment is at #{Apartment.first.address}."
 puts "It has  #{Apartment.first.tenants.count} tenants."
 
+
+class Menu
+  def self.display
+    while 1
+      puts "Choose one of the following:"
+      puts "1 - Option 1"
+      puts "2 - Option 2"
+      puts "3 - Option 3"
+      input = gets.chomp
+      if ["1","2","3"].include? input
+        self.select input
+        break
+      else
+        puts "Invalid option."
+      end
+    end
+  end
+  def self.select number
+    puts "You selected #{number}"
+  end
+end
+
+Menu.display
+
 binding.pry
 
 puts "ignore this line, it's just here so the binding.pry above works"
