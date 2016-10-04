cb5fd0696da6a4d37126d247e37e023bc6d0c7c3
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-10-04 17:20:11 -0400

    created connection file

---
+++ db/connection.rb
@@ -0,0 +1,18 @@
+require "pg" # postgres db library
+require "active_record" # the ORM
+require "pry"
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => "landlord"
+)
+
+if defined? Sinatra
+	after do
+	  ActiveRecord::Base.connection.close
+	end
+end
+
+binding.pry
+puts "end"
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+require "sinatra"
+require "sinatra/reloader"
+require "active_record"
+
+require_relative "db/connection"
+require_relative "models/apartment"
+require_relative "models/tenant"
