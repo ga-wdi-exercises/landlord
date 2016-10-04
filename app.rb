b43f9b42a3cd9a5b4e5a8be4576d0630d8acbed6
Tim Foley <thetimfoley@gmail.com>
2016-10-04 19:09:01 -0400

    added basic READ views

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+# Load the file to connect to the DB
+require_relative 'db/connection'
+
+# Load models
+require_relative 'models/apartment'
+require_relative 'models/tenant'
+
+get '/' do
+  erb :index
+end
+
+get '/apartments' do
+  @apartments = Apartment.all
+  erb :"apartments/index"
+end
+
+get '/apartments/:id' do
+  @apt = Apartment.find(params[:id])
+  @tenants = @apt.tenant
+  erb :"apartments/show"
+end
+
+get '/tenants' do
+  @tenants = Tenant.all
+  erb :"tenants/index"
+end
+
+get '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  erb :"tenants/show"
+end
