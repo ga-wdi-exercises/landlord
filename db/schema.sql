79ad21d16918f9ef54b8ae3b28d219e01fa5d130
Donovan Allen <dallen.928@gmail.com>
2016-10-31 23:36:41 -0400

    created database

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,6 @@
-DROP TABLE IF EXISTS tenants
-DROP TABLE IF EXISTS apartments
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
 
-CREATE TABLE tenants(
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  age INT,
-  gender TEXT,
-  apartment_id INT
-);
 
 CREATE TABLE apartments(
   id SERIAL PRIMARY KEY,
@@ -16,4 +9,12 @@ CREATE TABLE apartments(
   sqft INT,
   num_beds INT,
   num_baths INT
+);
+
+CREATE TABLE tenants(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INT,
+  gender TEXT,
+  apartment_id INT
 );
