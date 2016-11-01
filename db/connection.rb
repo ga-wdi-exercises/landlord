636fbfc6c87435ac004c27917dbf902cc591add1
robert <robert.k25@gmail.com>
2016-11-01 11:13:57 -0400

    adds connection cleanup

--- db/connection.rb
+++ db/connection.rb
@@ -3,3 +3,10 @@ ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
   :database => "landlord"
 )
+
+
+if defined? Sinatra
+	after do
+	  ActiveRecord::Base.connection.close
+	end
+end
