ad042bc7bb32de31a3554022686381752a57a4a0
robert <robert.k25@gmail.com>
2016-11-01 10:00:01 -0400

    completes ar exercises

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -71,19 +71,41 @@ apartments.each { |apartment|
 # Create 3 new apartments, and save them to the DB
 # Create at least 9 new tenants and save them to the DB. (Make sure they belong to an apartment)
 # Note: you'll use this little bit of code as a `seeds.rb` file later on.
+new_apartments = []
+
+for i in (0..2)
+  apartment = Apartment.new(address: "#{rand(0..1000)} Cherry St", monthly_rent: i+1 * 500, num_beds: i+1, num_baths: i+1, sqft: i+1 * 1000)
+  apartment.save
+  new_apartments << apartment
+end
+
+new_apartments.each { |apartment|
+  3.times {
+    apartment.tenants.create(name: "Renter #{rand(1..1000)}", age: rand(18..50), gender: ["Male", "Female"].sample)
+  }
+}
 
 # Birthday!
 # It's Kristin Wisoky's birthday. Find her in the DB and change her age to be 1 year older
 # Note: She's in the seed data, so she should be in your DB
+kristin = Tenant.where(name:"Kristin Wisoky").first
+kristin.age += 1
+kristin.save
+
 
 # Rennovation!
 # Find the apartment "62897 Verna Walk" and update it to have an additional bedroom
 # Make sure to save the results to your database
 
+verna_app = Apartment.find_by(address: "62897 Verna Walk")
+
+verna_app.num_beds += 1
 # Rent Adjustment!
 # Update the same apartment that you just 'rennovated'. Increase it's rent by $400
 # to reflect the new bedroom
-
+verna_app.monthly_rent += 400
+verna_app.save
 # Millenial Eviction!
 # Find all tenants who are under 30 years old
 # Delete their records from the DB
+Tenant.destroy_all("age < 30")
