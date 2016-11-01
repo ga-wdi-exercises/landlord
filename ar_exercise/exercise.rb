35c145cfa48512bd5dfb6661c246c833bbde6019
Angel Valant <angelgithub@gmail.com>
2016-10-31 21:49:04 -0400

    updates homework step 3

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -75,7 +75,24 @@ end
 # Hint, the following methods will help: `new`, `create`, `save`, `uddate`, `destroy`
 
 # Create 3 new apartments, and save them to the DB
+funnie_house = Apartment.create(address: "123 Bluffington Ave", monthly_rent: 2000, sqft: 2000, num_beds: 4, num_baths: 2)
+
+valentine_house = Apartment.create(address: "456 Bluffington Ave", monthly_rent: 1900, sqft: 2500, num_beds: 5, num_baths: 2)
+
+mayonnaise_house = Apartment.create(address: "987 Bluffington Ave", monthly_rent: 1600, sqft: 800, num_beds: 2, num_baths: 1)
 # Create at least 9 new tenants and save them to the DB. (Make sure they belong to an apartment)
+
+funnie_house.tenants.create(name: "Doug Funnie", age: 11, gender: "Male")
+funnie_house.tenants.create(name: "Phil Funnie", age: 43, gender: "Male")
+funnie_house.tenants.create(name: "Judy Funnie", age: 15, gender: "Female")
+funnie_house.tenants.create(name: "Theda Funnie", age: 43, gender: "Female")
+funnie_house.tenants.create(name: "Cleopatra Funnie", age: 1, gender: "Female")
+
+mayonnaise_house.tenants.create(name: "Patti Mayonnaise", age: 11, gender: "Female")
+mayonnaise_house.tenants.create(name: "Chad Mayonnaise", age: 40, gender: "Male")
+
+valentine_house.tenants.create(name: "Skeeter Valentine", age: 11, gender: "Male")
+valentine_house.tenants.create(name: "Dale Valentine", age: 1, gender: "Male")
 # Note: you'll use this little bit of code as a `seeds.rb` file later on.
 
 # Birthday!
