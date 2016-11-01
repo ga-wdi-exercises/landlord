02de3d493c993e13bb1e69f91e02d34735c0dc34
robert <robert.k25@gmail.com>
2016-11-01 10:25:24 -0400

    adds basic cli

---
+++ cli.rb
@@ -0,0 +1,21 @@
+require "active_record"
+require "pry"
+require_relative "models/apartment"
+require_relative "models/tenant"
+require_relative "db/connection"
+
+loop {
+  puts "Welcome, Bob. Property Management Menu"
+  puts "1 - View All Apartments"
+  puts "2 - View All Tenets"
+  puts "0 - Exit the Program"
+  option = gets.chomp.to_i
+  break if option == 0
+  if option == 1
+    puts Apartment.all.inspect
+  elsif option == 2
+    puts Tenant.all.inspect
+  else
+    puts "Unrecognized Option: #{option}"
+  end
+}
