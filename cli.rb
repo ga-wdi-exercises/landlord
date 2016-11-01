6e3f28d44555975255cfb8ebb6a8a0d02a27fb95
robert <robert.k25@gmail.com>
2016-11-01 15:32:02 -0400

    adds view all apartments and tenants to cli.rb

--- cli.rb
+++ cli.rb
@@ -8,6 +8,7 @@ loop {
   puts "Welcome, Bob. Property Management Menu"
   puts "1 - View All Apartments"
   puts "2 - View All Tenets"
+  puts "3 - View all Apartments "
   puts "0 - Exit the Program"
   option = gets.chomp.to_i
   break if option == 0
@@ -15,6 +16,13 @@ loop {
     puts Apartment.all.inspect
   elsif option == 2
     puts Tenant.all.inspect
+  elsif option == 3
+    Apartment.all.each { |apartment|
+      puts apartment.inspect
+      apartment.tenants.each { |tenant|
+        puts tenant.inspect
+      }
+    }
   else
     puts "Unrecognized Option: #{option}"
   end
