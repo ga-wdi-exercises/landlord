e5a856d988a02567742ec50a153e77c0ee55dc24
Angel Valant <angelgithub@gmail.com>
2016-11-02 22:16:19 -0400

    updates homework

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -31,42 +31,41 @@ end
 
 # Use Active record to do the following, and store the results **in a variable**
 # example: get every tenant in the DB
-# all_tenants = Tenant.all
+all_tenants = Tenant.all
 
 # get the first tenant in the DB
-# first_tenant = Tenant.first
+first_tenant = Tenant.first
 
 # get all tenants older than 65
-# older_tenants = Tenant.where(:age > 65)
+older_tenants = Tenant.where("age > 65", :age)
 
 # get all apartments whose price is greater than $2300
-# expensive_apartments = Apartment.where(:monthly_rent > 2300)
+expensive_apartments = Apartment.where("monthly_rent > 2300", :monthly_rent)
 
 # get the apartment with the address "6005 Damien Corners"
-# damien_apartment = Apartment.where(:address = "6005 Damien Corners")
+damien_apartment = Apartment.where("address = '6005 Damien Corners'", :address)
 
 # get all tenants in that apartment
-# damien_tenants = Apartment.where(:address = "6005 Damien Corners").tenants
+damien_tenants = Apartment.find_by(:address = "6005 Damien Corners").tenants
 
 # Use `each` and `puts` to:
 # Display the name and ID # of every tenant
-# Tenant.all.each do |tenant|
-#   puts "Name: #{tenant.name}, ID: #{tenant.id}"
-# end
+Tenant.all.each do |tenant|
+  puts "Name: #{tenant.name}, ID: #{tenant.id}"
+end
 
 # Iterate over each apartment, for each apartment, display it's address and rent price
-# Apartment.all.each do |apartment|
-#   puts "Address: #{apartment.address}, Rent: #{apartment.monthly_rent}"
-# end
+Apartment.all.each do |apartment|
+  puts "Address: #{apartment.address}, Rent: #{apartment.monthly_rent}"
+end
 
 # Iterate over each apartment, for each apartment, display it's address and all of it's tenants
-# Apartment.all.each do |apartment|
-#   puts apartment.address
-#   apartment.tenants.each do |tenant|
-#     puts tenant.name
-#   end
-#   puts "-" * 50
-# end
+Apartment.all.each do |apartment|
+  puts apartment.address
+  apartment.tenants.each do |tenant|
+    puts tenant.name
+  end
+end
 
 ################################################
 # CREATING / UPDATING / DELETING
@@ -75,29 +74,33 @@ end
 # Hint, the following methods will help: `new`, `create`, `save`, `uddate`, `destroy`
 
 # Create 3 new apartments, and save them to the DB
-# funnie_house = Apartment.create(address: "123 Bluffington Ave", monthly_rent: 2000, sqft: 2000, num_beds: 4, num_baths: 2)
+funnie_house = Apartment.create(address: "123 Bluffington Ave", monthly_rent: 2000, sqft: 2000, num_beds: 4, num_baths: 2)
 
-# valentine_house = Apartment.create(address: "456 Bluffington Ave", monthly_rent: 1900, sqft: 2500, num_beds: 5, num_baths: 2)
+valentine_house = Apartment.create(address: "456 Bluffington Ave", monthly_rent: 1900, sqft: 2500, num_beds: 5, num_baths: 2)
 
-# mayonnaise_house = Apartment.create(address: "987 Bluffington Ave", monthly_rent: 1600, sqft: 800, num_beds: 2, num_baths: 1)
+mayonnaise_house = Apartment.create(address: "987 Bluffington Ave", monthly_rent: 1600, sqft: 800, num_beds: 2, num_baths: 1)
 # Create at least 9 new tenants and save them to the DB. (Make sure they belong to an apartment)
 
-# funnie_house.tenants.create(name: "Doug Funnie", age: 11, gender: "Male")
-# funnie_house.tenants.create(name: "Phil Funnie", age: 43, gender: "Male")
-# funnie_house.tenants.create(name: "Judy Funnie", age: 15, gender: "Female")
-# funnie_house.tenants.create(name: "Theda Funnie", age: 43, gender: "Female")
-# funnie_house.tenants.create(name: "Cleopatra Funnie", age: 1, gender: "Female")
-#
-# mayonnaise_house.tenants.create(name: "Patti Mayonnaise", age: 11, gender: "Female")
-# mayonnaise_house.tenants.create(name: "Chad Mayonnaise", age: 40, gender: "Male")
-#
-# valentine_house.tenants.create(name: "Skeeter Valentine", age: 11, gender: "Male")
-# valentine_house.tenants.create(name: "Dale Valentine", age: 1, gender: "Male")
+funnie_house.tenants.create(name: "Doug Funnie", age: 11, gender: "Male")
+funnie_house.tenants.create(name: "Phil Funnie", age: 43, gender: "Male")
+funnie_house.tenants.create(name: "Judy Funnie", age: 15, gender: "Female")
+funnie_house.tenants.create(name: "Theda Funnie", age: 43, gender: "Female")
+funnie_house.tenants.create(name: "Cleopatra Funnie", age: 1, gender: "Female")
+
+mayonnaise_house.tenants.create(name: "Patti Mayonnaise", age: 11, gender: "Female")
+mayonnaise_house.tenants.create(name: "Chad Mayonnaise", age: 40, gender: "Male")
+
+valentine_house.tenants.create(name: "Skeeter Valentine", age: 11, gender: "Male")
+valentine_house.tenants.create(name: "Dale Valentine", age: 1, gender: "Male")
+
 # Note: you'll use this little bit of code as a `seeds.rb` file later on.
 
 # Birthday!
 # It's Kristin Wisoky's birthday. Find her in the DB and change her age to be 1 year older
 # Note: She's in the seed data, so she should be in your DB
+kristin = Tenant.find_by(name: "Kristin Wisoky").increment(:age, 1).save
+kristin.age +=1
+kristin.save
 
 # Rennovation!
 # Find the apartment "62897 Verna Walk" and update it to have an additional bedroom
@@ -106,7 +109,10 @@ end
 # Rent Adjustment!
 # Update the same apartment that you just 'rennovated'. Increase it's rent by $400
 # to reflect the new bedroom
+verna_walk.update(monthly_rent: 2800)
+
 
 # Millenial Eviction!
 # Find all tenants who are under 30 years old
 # Delete their records from the DB
+Tenant.destroy_all("age < 30")
