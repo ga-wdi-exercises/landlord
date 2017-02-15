88de88236b3f5dfd061c62365c0eb25709a1afc0
Vince Campanale <vcampanale0294@gmail.com>
2017-02-15 11:58:22 -0500

    can view an apartments details and go to an edit page

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -2,3 +2,13 @@ get '/apartments' do
   @apartments = Apartment.all
   erb :'apartments/index'
 end
+
+get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :'apartments/individual_apartment'
+end
+
+get '/apartments/:id/edit' do
+  @apartment = Apartment.find(params[:id])
+  erb :'apartments/edit'
+end
