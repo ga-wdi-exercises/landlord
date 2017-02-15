3107d42e2cbacc6160e8b520e253772d58b0498c
Eva Lai <evalai.dc@gmail.com>
2017-02-15 12:31:28 -0500

    created home page with sinatra

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+require 'active_record'
+
+require_relative "db/connection"
+require_relative "models/apartment"
+require_relative "models/tenant"
+
+
+get '/' do
+ erb :home
+end
+
+get '/apartments' do
+  @apartments = Apartment.all
+  erb :"apartments/apartments"
+end
+
+get 'apartments/new' do
+  erb :"apartments/new"
+end
+
+post '/apartments' do
+  @aparment = Apartment.create(address: params[:address], monthly_rent: params[:monthly_rent], sqft: params[:sqft], num_beds: params[:num_beds], num_baths: params[:num_baths])
+  redirect "apartments/#{@apartment.id}"
+end
+get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/show"
+end
