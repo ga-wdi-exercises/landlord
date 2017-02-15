a6638fdaa45053484ba0db10cbf4bcd87d91f076
Eva Lai <evalai.dc@gmail.com>
2017-02-15 09:48:58 -0500

    destroyed all previous apt and tentants and created new records

--- console.rb
+++ console.rb
@@ -3,17 +3,15 @@ require_relative "models/apartment"
 require_relative "models/tenant"
 
 
-the_bat_cave = Apartment.create(address: "123 Main St", monthly_rent: 2000, sqft: 600, num_beds: 2, num_baths: 1)
-me = Tenant.create(name: "Adam", age: 30, gender: "Male", apartment: the_bat_cave)
-# NOTE: Uncomment the lines below to verify that your seed script is working
+# : Uncomment the lines below to verify that your seed script is working
 
-# puts "There are #{Apartment.count} apartments"
-# puts "There are #{Tenant.count} tenants"
-#
-# puts "*" * 50
-#
-# puts "The first apartment is at #{Apartment.first.address}."
-# puts "It has  #{Apartment.first.tenants.count} tenants."
+puts "There are #{Apartment.count} apartments"
+puts "There are #{Tenant.count} tenants"
+
+puts "*" * 50
+
+puts "The first apartment is at #{Apartment.first.address}."
+puts "It has  #{Apartment.first.tenants.count} tenants."
 
 binding.pry
 
