a9bb07ddeb6df04110b4d203cf6fc778b397f890
Vince Campanale <vcampanale0294@gmail.com>
2017-02-15 12:13:08 -0500

    can edit an apartment

--- controllers/apartments.rb
+++ controllers/apartments.rb
@@ -12,3 +12,13 @@ get '/apartments/:id/edit' do
   @apartment = Apartment.find(params[:id])
   erb :'apartments/edit'
 end
+
+put '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
+end
+
+post '/apartments/:id' do
+  
+end
