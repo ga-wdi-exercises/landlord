911e530b8f556799fb36235548960fc6245c85be
Natasha Clark <Natasharclark@gmail.com>
2017-02-15 13:52:40 -0500

    mostly done

--- console.rb
+++ console.rb
@@ -3,14 +3,14 @@ require_relative "models/apartment"
 require_relative "models/tenant"
 
 
-puts "There are #{Apartment.count} apartments"
-puts "There are #{Tenant.count} tenants"
-
-puts "*" * 50
-
-puts "The first apartment is at #{Apartment.first.address}."
-puts "It has  #{Apartment.first.tenants.count} tenants."
-
-binding.pry
-
-puts "ignore this line, it's just here so the binding.pry above works"
+# puts "There are #{Apartment.count} apartments"
+# puts "There are #{Tenant.count} tenants"
+#
+# puts "*" * 50
+#
+# puts "The first apartment is at #{Apartment.first.address}."
+# puts "It has  #{Apartment.first.tenants.count} tenants."
+#
+# binding.pry
+#
+# puts "ignore this line, it's just here so the binding.pry above works"
