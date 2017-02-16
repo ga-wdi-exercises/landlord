172ea6ec374efd481a6f92c1f55beb66ca69d69e
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-02-15 20:15:28 -0500

    CLI interface

--- console.rb
+++ console.rb
@@ -15,3 +15,17 @@ require_relative "models/tenant"
 binding.pry
 
 puts "ignore this line, it's just here so the binding.pry above works"
+
+# CLI Interface commands (Step 4)
+# 1. Apartment.all.each do |ID, address, monthly_rent|
+#       puts apartment.apartment_id
+#       puts apartment.address
+#       puts apartment.monthly_rent
+#     end
+# 2. Tenant.all.each do |name, id|
+#       puts tenant.name
+#       puts tenant.id
+#     end
+# 3. Apartment.all.each do |apartment|
+#       puts #{apartment.address} #{Tenant.find_by(apartment_id: apartment.id) ? Tenant.find_by(apartment_id: apartment.id).name
+#     end
