88a2ccee40b6a280a14c1793c6d82f1f13cc87ff
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-02-16 08:26:22 -0500

    sinatra

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@
 require "pg" # postgres db library
 require "active_record" # the ORM
 require "pry" # for debugging
-
+require "sinatra"
+require "sinatra/reloader"
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
   :database => "landlord"
@@ -19,26 +20,39 @@ class Apartment < ActiveRecord::Base
   has_many :tenants
 end
 
+# CLI Commands
+# puts "OPTIONS"
+# puts "Type '1' to view all the apartments"
+# puts "Type '2' to view all the tenants"
+# puts "Type '3' to view list of all apartments and their associated tenants"
+#
+# user_input = gets.chomp
+# if user_input == "1"
+#   # apartment.each do |apt|
+#   #   puts apt
+#   # end
+#   puts Apartment.all
+# elsif user_input == "2"
+#   # tenant.each do |tenant|
+#   #   puts tenant
+#   puts Tenant.all
+#
+# elsif user_input == "3"
+#   Apartment.all.each do |apartment|
+#                  house_tenants = Tenant.select{|tenant| tenant[:apartment_id] == apartment[:id]}
+#                  puts apartment.address
+#                  puts house_tenants
+#               end
+#
+# end
 
-puts "OPTIONS"
-puts "Type '1' to view all the apartments"
-puts "Type '2' to view all the tenants"
-puts "Type '3' to view list of all apartments and their associated tenants"
 
-user_input = gets.chomp
-if user_input == "1"
-  apartment.each do |apt|
-    puts apt
-  end
-elsif user_input == "2"
-  tenant.each do |tenant|
-    puts tenant
-  end
-elsif user_input == "3"
-  tenant.each do |tenant|
-    puts tenant
-  end
+# binding.pry
+
+get '/' do
+  erb :index
 end
 
-
-binding.pry
+get '/apartments' do
+  erb :"apartments/index"
+end
