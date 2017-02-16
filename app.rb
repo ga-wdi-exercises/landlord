e8a6c8436dd4be752cc0e6ababe7761b5e791e34
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-15 19:29:02 -0500

    finished sinatra and active code

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ require_relative 'db/connection'
 require_relative 'models/apartment'
 require_relative 'models/tenant'
 
+get '/' do
+  erb :"home"
+end
+
 get '/apartments' do
   @apartments = Apartment.all
   erb :"apartments/index"
@@ -24,4 +28,43 @@ end
 get '/apartments/:id' do
   @apartment = Apartment.find(params[:id])
   erb :"apartments/show"
+end
+
+get '/apartments/:id/edit' do
+  @apartment = Apartment.find(params[:id])
+  erb(:"apartments/edit")
+end
+
+get '/apartments/:id/tenants/new' do
+  @apartment = Apartment.find(params[:id])
+  erb :"tenants/new"
+end
+
+post '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
+  @tenant = Tenant.create(name: params[:name], age: params[:age], gender: params[:gender], apartment_id: @apartment[:id])
+  redirect "/apartments/#{@apartment.id}/tenants/#{@tenant.id}"
+end
+
+get '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
+  @tenants = Tenant.select{|tenant| tenant[:apartment_id] == @apartment[:id]}
+  erb :"/apartments/tenants"
+end
+
+get '/apartments/:id/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  erb :"tenants/show"
+end
+
+put '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect("/apartments/#{@apartment.id}")
+end
+
+delete '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.destroy
+  redirect("/apartments")
 end
