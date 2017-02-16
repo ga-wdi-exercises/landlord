e0b534a79e3c35be4050a4795cc867664bdd8c3a
Ebenezer Williams <ebenezerw@me.com>
2017-02-16 00:04:53 -0500

    can view all apartments, add, edit, delete and view tenants

--- app.rb
+++ app.rb
@@ -6,53 +6,97 @@ require_relative 'db/connection'
 require_relative 'models/apartment'
 require_relative 'models/tenant'
 
+#
+# puts "Welcome to my Apartments"
+#
+# active_apartment = false
+#
+# while active_apartment = true
+#   puts "Enter 1 to view all apartments"
+#   puts "Enter 2 to view all tenants"
+#   puts "Enter 3 to see where they live"
+#
+#   input = gets.chomp.to_i
+#   if input == 1
+#     puts "List of Apartments"
+#     Apartment.all.each do |apartment|
+#       puts "ID: #{apartment.id} Address: #{apartment.address} Rent #{apartment.monthly_rent}"
+#       puts "*" * 50
+#
+#     end
+#
+#   end
+#   if input == 2
+#     puts "List of Tenants"
+#     Tenant.all.each do |tenant|
+#       puts "Name: #{tenant.name}, Age: #{tenant.age}"
+#       puts "*" * 50
+#     end
+#
+#   end
+#   if input == 3
+#     puts "Apartments and their Tenants:"
+#     Apartment.all.each do |apartment|
+#       puts apartment.address
+#
+#       apartment.tenants.each do |tenant|
+#         puts tenant.name
+#
+#       end
+#       puts "*" * 50
+#     end
+#   end
+# end
+#
+# binding.pry
 
-puts "Welcome to my Apartments"
+#show all apartments on index page
 
-active_apartment = false
+get '/apartments' do
+  @apartments = Apartment.all
+  erb :"apartments/index"
+end
 
-while active_apartment = true
-  puts "Enter 1 to view all apartments"
-  puts "Enter 2 to view all tenants"
-  puts "Enter 3 to see where they live"
+get '/apartments/new' do
+  erb :"apartments/new"
+end
+# show information on individual apartments
 
-  input = gets.chomp.to_i
-  if input == 1
-    puts "List of Apartments"
-    Apartment.all.each do |apartment|
-      puts "ID: #{apartment.id} Address: #{apartment.address} Rent #{apartment.monthly_rent}"
-      puts "*" * 50
+get '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  erb :"apartments/show"
+end
 
-    end
+get '/apartments/:id/tenants' do
+  @apartment = Apartment.find(params[:id])
+  @tenants = Tenant.select{|tenant| tenant[:apartment_id] == @apartment[:id]}
+  erb :"/apartments/tenants"
+end
+# create new apartments
 
-  end
-  if input == 2
-    puts "List of Tenants"
-    Tenant.all.each do |tenant|
-      puts "Name: #{tenant.name}, Age: #{tenant.age}"
-      puts "*" * 50
-    end
-
-  end
-  if input == 3
-    puts "Apartments and their Tenants:"
-    Apartment.all.each do |apartment|
-      puts apartment.address
-
-      apartment.tenants.each do |tenant|
-        puts tenant.name
-
-      end
-      puts "*" * 50
-    end
-  end
+post '/apartments' do
+  @apartment = Apartment.create(params[:apartment])
+  redirect "/apartments/#{@apartment.id}"
 end
 
 
+# edit apartment
 
+get "/apartments/:id/edit" do
+  @apartment = Apartment.find(params[:id])
+  erb(:"apartments/edit")
+end
 
+put '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.update(params[:apartment])
+  redirect("/apartments/#{@apartment.id}")
+end
 
+#delete apartment
 
-
-
-binding.pry
+delete '/apartments/:id' do
+  @apartment = Apartment.find(params[:id])
+  @apartment.destroy
+  redirect("/apartments")
+end
