55340fda2098a808031f0eb5ecc08e069feaacec
Collin OConnell <ococncol@gmail.com>
2017-02-15 21:59:48 -0500

    did ar_exercises

--- ar_exercise/exercise.rb
+++ ar_exercise/exercise.rb
@@ -36,14 +36,28 @@ all_tenants = Tenant.all
 # get the first tenant in the DB
 first_tendant = Apartment.first.tenants
 # get all tenants older than 65
+older_65 = Tenant.where('age > 65', :age)
 # get all apartments whose price is greater than $2300
+over_2300 = Apartment.where('monthly_rent > 2300', :monthly_rent)
 # get the apartment with the address "6005 Damien Corners"
+add_apt = Apartment.find_by address: "6005 Damien Corners"
 # get all tenants in that apartment
-
+apt6005_tenants = Tenant.where("apartment_id = 6", :apartment_id)
 # Use `each` and `puts` to:
 # Display the name and ID # of every tenant
+Tenant.all.each do |tenant|
+  puts tenant.name, tenant.id
+end
 # Iterate over each apartment, for each apartment, display it's address and rent price
+Apartment.all.each do |apartment|
+  puts apartment.address, apartment.monthly_rent
+end
 # Iterate over each apartment, for each apartment, display it's address and all of it's tenants
+Apartment.all.each do |apartment|
+  apt_tenants = Tenant.select{|tenant| tenant[:apartment_id] == apartment[:id]}
+  puts apartment.address
+  puts apt_tenants
+end
 
 ################################################
 # CREATING / UPDATING / DELETING
@@ -75,15 +89,23 @@ Apartment.all[2].tenants.create([
 # Birthday!
 # It's Kristin Wisoky's birthday. Find her in the DB and change her age to be 1 year older
 # Note: She's in the seed data, so she should be in your DB
+kristin_bd = Tenant.find_by name: "Kristin Wisoky"
+kristin_bd.update age: 24
 
 # Rennovation!
 # Find the apartment "62897 Verna Walk" and update it to have an additional bedroom
 # Make sure to save the results to your database
+verna = Apartment.find_by address: "62897 Verna Walk"
+verna.update num_beds: 3
+verna.save
 
 # Rent Adjustment!
 # Update the same apartment that you just 'rennovated'. Increase it's rent by $400
 # to reflect the new bedroom
+verna.update monthly_rent: 2800;
+verna.save
 
 # Millenial Eviction!
 # Find all tenants who are under 30 years old
 # Delete their records from the DB
+Tenant.where("age < 30", :age).destroy_all
