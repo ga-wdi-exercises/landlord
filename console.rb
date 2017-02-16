c4d2547540e557beef628b33ce7ce3a768ed1418
Dominick Houston <dominick.houston14@gmail.com>
2017-02-15 21:03:04 -0500

    Commit #4 CLI interface

--- console.rb
+++ console.rb
@@ -6,13 +6,49 @@ require "pry" # for debugging
 
 # NOTE: Uncomment the lines below to verify that your seed script is working
 
-puts "There are #{Apartment.count} apartments"
-puts "There are #{Tenant.count} tenants"
+# puts "There are #{Apartment.count} apartments"
+# puts "There are #{Tenant.count} tenants"
+#
+# puts "*" * 50
+#
+# puts "The first apartment is at #{Apartment.first.address}."
+# puts "It has  #{Apartment.first.tenants.count} tenants."
 
-puts "*" * 50
 
-puts "The first apartment is at #{Apartment.first.address}."
-puts "It has  #{Apartment.first.tenants.count} tenants."
+def menu
+  puts "please choose a menu item Number"
+  puts "1. Apartments"
+  puts "2. Tenants"
+  puts "3. Tenants by Apartment"
+  puts "Close"
+end
+
+user_input = gets.chomp.to_s
+
+user_input = menu
+
+while input != "close"
+  if user_input == 1
+    puts "Apartments in Building:"
+
+    Apartment.each do |apartment|
+      puts "#{apartment.address}, #{apartment.monthly_rent}, #{apartment.sqft}, #{apartment.num_beds}, #{apartment.num_baths}"
+    end
+
+  elsif user_input == 2
+    puts "Tenants in Building"
+    Tenant.each do |tenants|
+      puts "#{tenants.name}, #{tenants.age}, #{tenants.gender}, #{tenants.apartment_id}"
+    end
+  elsif user_input == 3
+    puts "Tenants by Apartments"
+    Apartment.all.each do |apartment|
+      puts "#{apartment.address}"
+    apartment.tenants.each do |tenants|
+        puts "#{tenants.name}"
+      end
+    end
+  end
 
 binding.pry
 
