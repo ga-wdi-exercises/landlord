7ed4aa26c64bec2d9f21726f620bcae92e34a4f0
Dan Lin <dlin2102@gmail.com>
2017-02-15 23:27:47 -0500

    commit4

--- console.rb
+++ console.rb
@@ -4,13 +4,34 @@ require_relative "models/tenant"
 
 # NOTE: Uncomment the lines below to verify that your seed script is working
 
- puts "There are #{Apartment.count} apartments"
-puts "There are #{Tenant.count} tenants"
+puts "Welcome to Roosevelt Apartments"
+puts "Select 1 for a list of all apartments"
+puts "Select 2 for a list of all tenants"
+puts "Select 3 for a list of all apartments and their tenants"
 
-puts "*" * 50
+input = gets.chomp.to_i
 
-puts "The first apartment is at #{Apartment.first.address}."
-puts "It has  #{Apartment.first.tenants.count} tenants."
+while input == 1 || 2 || 3
+  if input == 1
+    Apartment.each do |apartment|
+    puts "#{apartment.address}, #{apartment.monthly_rent}, #{apartment.sqft}, #{apartment.num_beds}, #{apartment.num_baths}"
+  end
+elsif input == 2
+Tenant.each do |tenant|
+  puts "#{tenant.name}, #{tenant.age}"
+end
+elsif input == 3
+  #not sure how to complete
+    end
+  end
+end
+else
+  puts "Invalid entry, please try again."
+end
+
+end
+
+
 
 binding.pry
 
