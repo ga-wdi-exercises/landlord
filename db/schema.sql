f02777c56523158fbbb47d7afa94dc162b1c1d55
Jonathan Wright <jonwright.va@gmail.com>
2017-04-14 14:22:07 -0400

    database seeded

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
+
 CREATE TABLE tenants(
   id SERIAL PRIMARY KEY,
   name TEXT,
@@ -11,6 +14,6 @@ CREATE TABLE apartments(
   address TEXT,
   monthly_rent INT,
   sqft INT,
-  num_beds INT
+  num_beds INT,
   num_baths INT
 );
