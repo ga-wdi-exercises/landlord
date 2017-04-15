91d84d1c35460010dcf4a17dd6e285b78d1e8a1a
Whitney <whitney.purdum@gmail.com>
2017-04-15 18:14:45 -0400

    completed exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
-require 'active_record'
-require 'pry'
+
 
 require_relative "../models/apartment"
 require_relative "../models/tenant"
 
-require_relative "../db/connection.rb
+require_relative "/connection.rb"
 
+
+At the very top, ensure your seed file loads the necessary files using `require_relative`:
+* the connection file `db/connection`
+* the 2 model files
+
+Below that, add these two lines, to ensure running the seed script clears your DB first.
+```ruby
 Tenant.destroy_all
 Apartment.destroy_all
+```
