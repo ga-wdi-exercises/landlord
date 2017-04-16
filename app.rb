8a616ae881e0b827bef4d6a7358119e6b536341f
Jonathan Wright <jonwright.va@gmail.com>
2017-04-15 22:28:43 -0400

    added views, started templating

--- app.rb
+++ app.rb
@@ -1,14 +1,50 @@
+# require "pg" # postgres db library
+# require "active_record" # the ORM
+# require "pry" # for debugging
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/' do
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "postgresql",
+#   :database => "landlord"
+# )
+#
+# class Tenant < ActiveRecord::Base
+#   belongs_to :apartment
+# end
+#
+# class Apartment < ActiveRecord::Base
+#   has_many :tenants
+# end
 
+
+apartments_list = [{address: "place0", id: 1},
+              {address: "place1", id: 2},
+              {address: "place2", id: 3},
+              {address: "place3", id: 4},
+              {address: "place4", id: 5}]
+
+
+get '/' do
+    erb :index
 end
 
 get '/apartments' do
-
+    @apartments = apartments_list
+    erb :apartments
 end
 
-get '/tentants' do
-  
+get '/apartments/:num' do
+    @apartment_id = params[:id].to_i
+    erb :index
+end
+
+get '/apartments/new' do
+
+    erb :index
+end
+
+get '/apartments/1/tenants' do
+
+    erb :index
 end
