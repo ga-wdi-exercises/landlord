87aa7ff5c46b4f56780a2bca85e37ad04484483c
Erin <erinedawson@gmail.com>
2017-04-17 05:39:58 -0400

    add CLI interface functionality.  not working

--- console.rb
+++ console.rb
@@ -9,13 +9,29 @@ require_relative "models/tenant"
 
 # NOTE: Uncomment the lines below to verify that your seed script is working
 
-puts "There are #{Apartment.count} apartments"
-puts "There are #{Tenant.count} tenants"
+# puts "There are #{Apartment.count} apartments"
+# puts "There are #{Tenant.count} tenants"
+#
+# puts "*" * 50
+#
+# puts "The first apartment is at #{Apartment.first.address}."
+# puts "It has  #{Apartment.first.tenants.count} tenants."
 
-puts "*" * 50
 
-puts "The first apartment is at #{Apartment.first.address}."
-puts "It has  #{Apartment.first.tenants.count} tenants."
+puts "Welcome to the Landlord app!"
+puts "Press 1 for a list of your apartments.  Press 2 for a list of your tenants.  Press 3 to see who lives where."
+user_input = gets.chomp.to_i
+if (user_input == 1)
+  puts Apartment.all
+end
+elsif (user_input == 2)
+  puts Tenant.all
+end
+elsif (user_input == 3)
+     puts Tenant.where(apartment_id == Apartment(id))
+end
+end
+
 
 binding.pry
 
