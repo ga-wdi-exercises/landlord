dbdf332c6e6d04ec3229d3cbf7cbf2c4fe6c5609
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-22 17:57:34 -0400

    finshed Schema & SQL section

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tenants;
+DROP TABLE IF EXISTS apartments;
+
+CREATE TABLE tenants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  age INTEGER,
+  gender VARCHAR(50),
+  apartment_id INTEGER
+);
+
+CREATE TABLE apartments (
+  id SERIAL PRIMARY KEY,
+  address TEXT,
+  monthly_rent INTEGER,
+  sqft INTEGER,
+  num_beds INTEGER,
+  num_baths INTEGER
+);
