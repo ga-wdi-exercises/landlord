0dee4c1892ee791190598acae946a665369b2a8e
Perry Fustero <pjfust@gmail.com>
2017-07-26 19:33:31 -0400

    crud for tenants

--- app.rb
+++ app.rb
@@ -1,56 +1,61 @@
 require "pg" # postgres db library
 require "active_record" # the ORM
 require "pry" # for debugging
+require 'sinatra'
+require 'sinatra/reloader'
 
 require_relative "db/connection"
 require_relative "models/apartment"
 require_relative "models/tenant"
 
-ruby_apartments = Apartment.all
-ruby_tenants = Tenant.all
-
-run = true
-
-while run do
-
-  puts " "
-  puts "*************************************"
-  puts "Welcome Bob Pizza"
-  puts "Please Make your Selection"
-  puts "1 ==> View all apartments"
-  puts "2 ==> View all tenants"
-  puts "3 ==> View apartments with their tenants"
-  puts "0 ==> Exit Program"
-
-  input = gets.chomp.to_i
-
-  if input == 1
-    puts "Printing out apartment data..."
-    ruby_apartments.each do |i|
-      puts "ID: " + i.id.to_s
-      puts "Address: " + i.address.to_s
-      puts "Montly Rent: " + i.monthly_rent.to_s
-    end
-
-  elsif input == 2
-    puts "Printing out tenant data..."
-    ruby_tenants.each do |i|
-      puts "Name: " + i.name
-      puts "Age: " + i.name.to_s
-    end
-
-  elsif input == 3
-    puts "Printing out apartments with tenants..."
-    ruby_apartments.each do |apt|
-      puts apt[:address] + ": "
-      id = apt[:id]
-      match = ruby_tenants.find_all { |person| person[:apartment_id] == id}
-        match.each do |person|
-          puts "   " + person[:name]
-        end
-    end
-
-  elsif input == 0
-  run = false
+get '/' do #HOME PAGE
+  @tenants = Tenant.all
+  @apartments = Apartment.all
+  erb :"home"
 end
+
+get '/tenants' do #READ
+  @tenants = Tenant.all
+  erb :"tenants/index"
+end
+
+get '/tenants/new' do #CREATE
+  erb :"tenants/new"
+end
+
+post '/tenants' do #CREATE
+  @tenant = Tenant.create(params[:tenant])
+  redirect "/tenants/#{@tenant.id}"
+end
+
+get '/tenants/:id' do #READ
+  @tenant = Tenant.find(params[:id])
+  erb :"tenants/show"
+end
+
+get '/tenants/:id/edit' do
+  @tenant = Tenant.find(params[:id])
+  erb :"tenants/edit"
+end
+
+put '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  @tenant.update(params[:tenant])
+  redirect "/tenants/#{@tenant.id}"
+end
+
+delete '/tenants/:id' do
+  @tenant = Tenant.find(params[:id])
+  @tenant.destroy
+  redirect "/tenants"
+end
+
+# get '/pokemons/:id' do #READ
+#   @pokemon = Pokemon.find(params[:id])
+#   erb :"pokemons/show"
+# end
+
+get '/apartments' do
+  @apartments = Apartment.all
+  erb :"apartments/index"
 end
