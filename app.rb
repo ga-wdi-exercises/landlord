47e2cc8da356c105b29543d0efc516126d8aee60
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-26 21:13:12 -0400

    add CRUD

--- app.rb
+++ app.rb
@@ -17,10 +17,41 @@ get '/apartments' do
   erb :"apartments/index"
 end
 
+#new apartment
+get '/apartments/new' do
+  @apartments = Apartment.all
+  erb :"apartments/new"
+end
+
+#post apartment
+post '/apartments' do
+  @apartments = Apartment.create!(params[:apartments])
+  redirect "/apartments/#{@apartments.id}"
+end
+
 #apartment show page
 get '/apartments/:id' do
   @apartments = Apartment.find(params[:id])
   erb :"/apartments/show"
+end
+
+#edit apartment
+get '/apartments/:id/edit' do
+  @apartments = Apartment.find(params[:id])
+  erb :"apartments/edit"
+end
+
+#post edited apartment
+put '/apartments/:id' do
+  @apartments = Apartment.find(params[:id])
+  @apartments.update(params[:apartments])
+  redirect "/apartments/#{@apartments.id}"
+end
+
+delete '/apartments/:id' do
+  @apartments = Apartment.find(params[:id])
+  @apartments.destroy
+  redirect '/apartments'
 end
 
 # binding.pry
